a1d33a69cfaf951bfd43b5d13759ad64994beb83
Kurt Griffiths <kurt.griffiths@rackspace.com>
2013-02-04 12:27:59 -0500

    fix(Request): UTF-8 logging

--- tests/test_wsgi_errors.py
+++ tests/test_wsgi_errors.py
@@ -1,14 +1,13 @@
 import io
-import sys
+import six
 from . import helpers
-
 
 unicode_message = u'Unicode: \x80'
 
-if sys.version_info[0] == 2:
-    str_message = 'UTF-8: \xc2\x80'
-else:
+if six.PY3:
     str_message = 'Unicode all the way: \x80'
+else:
+    str_message = 'UTF-8: \xc2\x80'
 
 
 class BombResource:
@@ -37,31 +36,46 @@ class TestWSGIError(helpers.TestSuite):
 
         self.api.add_route('/bomb', self.tehbomb)
         self.api.add_route('/logger', self.tehlogger)
-        self.wsgierrors = io.StringIO()
+
+        self.wsgierrors_buffer = io.BytesIO()
+
+        if six.PY3:
+            # Simulate Gunicorn's behavior under Python 3
+            self.wsgierrors = io.TextIOWrapper(self.wsgierrors_buffer,
+                                               line_buffering=True)
+        else:
+            # WSGI servers typically present an open file object,
+            # with undefined encoding, so do the encoding manually.
+            self.wsgierrors = self.wsgierrors_buffer
 
     def test_exception_logged(self):
         self._simulate_request('/bomb', wsgierrors=self.wsgierrors)
-        log = self.wsgierrors.getvalue()
 
-        self.assertIn('IOError', log)
+        log = self.wsgierrors_buffer.getvalue()
+        self.assertIn(b'IOError', log)
 
     def test_exception_logged_with_details(self):
         self._simulate_request('/bomb', wsgierrors=self.wsgierrors,
                                method='HEAD')
-        log = self.wsgierrors.getvalue()
 
-        self.assertIn('MemoryError', log)
-        self.assertIn('remember a thing', log)
+        log = self.wsgierrors_buffer.getvalue()
+
+        self.assertIn(b'MemoryError', log)
+        self.assertIn(b'remember a thing', log)
 
     def test_responder_logged_unicode(self):
         self._simulate_request('/logger', wsgierrors=self.wsgierrors)
 
-        log = self.wsgierrors.getvalue()
-        self.assertIn(unicode_message, log)
+        log = self.wsgierrors_buffer.getvalue()
+        self.assertIn(unicode_message.encode('utf-8'), log)
 
     def test_responder_logged_str(self):
         self._simulate_request('/logger', wsgierrors=self.wsgierrors,
                                method='HEAD')
 
-        log = self.wsgierrors.getvalue()
-        self.assertIn(str_message, log)
+        log = self.wsgierrors_buffer.getvalue()
+
+        if six.PY3:
+            self.assertIn(str_message.encode('utf-8'), log)
+        else:
+            self.assertIn(str_message, log)
