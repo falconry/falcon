1f9e0f40469ea3e75f09e33387d05bafc86135c9
kgriffs <kurt.griffiths@rackspace.com>
2013-03-19 01:50:28 -0400

    chore: Bump version to 0.1.3-dev

--- falcon/version.py
+++ falcon/version.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 """
 
-VERSION_TUPLE = (0, 1, 2, '')
+VERSION_TUPLE = (0, 1, 3, '-dev')
 
 version = '.'.join(map(str, VERSION_TUPLE[:-1])) + VERSION_TUPLE[-1]
 """Current version of Falcon."""
