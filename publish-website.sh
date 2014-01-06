427c26635332a647402b13846afd52a18157d35f
kgriffs <kurt.griffiths@rackspace.com>
2014-01-06 12:03:56 -0600

    doc: Do not use compressed website docs

--- publish-website.sh
+++ publish-website.sh
@@ -7,12 +7,6 @@ then
     rm -rf js
     cp -r ../falconframework.org/* .
 
-    mv compressed/* ./
-    rm compressed
-
-    mv css/compressed/* css/
-    rm css/compressed
-
     git add *
     git commit -m 'doc: Publish website'
     git push origin gh-pages
