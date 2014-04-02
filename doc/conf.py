13a10a586bfae0b4538c47dde3777e1be39d94bf
Chris Petersen <geek@ex-nerd.com>
2014-04-01 23:11:57 -0700

    Add and enable doc req for sphinxcontrib.napoleon
    This provides better support for Google-style docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
