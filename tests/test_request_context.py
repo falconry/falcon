01edc1b0f24b0a20662b5cc59dd20b5346652736
Chris Petersen <geek@ex-nerd.com>
2014-05-02 11:59:42 -0700

    pep8 fixes

--- tests/test_request_context.py
+++ tests/test_request_context.py
@@ -2,6 +2,7 @@ import falcon.testing as testing
 
 from falcon.request import Request
 
+
 class TestRequestContext(testing.TestBase):
 
     def test_default_request_context(self):
@@ -10,21 +11,23 @@ class TestRequestContext(testing.TestBase):
         self.assertIsInstance(req.context, dict)
 
     def test_custom_request_context(self):
+
         # Define a Request-alike with a custom context type
         class MyCustomContextType():
             pass
+
         class MyCustomRequest(Request):
-            context_type=MyCustomContextType
+            context_type = MyCustomContextType
 
         env = testing.create_environ()
         req = MyCustomRequest(env)
         self.assertIsInstance(req.context, MyCustomContextType)
 
     def test_custom_request_context_failure(self):
+
         # Define a Request-alike with a non-callable custom context type
         class MyCustomRequest(Request):
-            context_type=False
+            context_type = False
 
         env = testing.create_environ()
         self.assertRaises(TypeError, MyCustomRequest, env)
-
