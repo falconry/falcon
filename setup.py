5cfe9ff9e523ee7654f8bb6c2fe0848c1855f0aa
Andy Freeland <andy.freeland@redjack.com>
2014-06-25 12:07:46 -0400

    fix(setup.py): Don't install 'tests' to site-packages

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     author_email='mail@kgriffs.com',
     url='http://falconframework.org',
     license='Apache 2.0',
-    packages=find_packages(exclude=['*.tests']),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIRES,
