2a5c215500c96875d4fc77a9910da2dbc0cf64fe
Kurt Griffiths <kurt.griffiths@rackspace.com>
2015-02-13 15:52:44 -0600

    chore: Bump version to 0.2.0rc2

--- falcon/version.py
+++ falcon/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.2.0rc1'
+__version__ = '0.2.0'
 """Current version of Falcon."""
