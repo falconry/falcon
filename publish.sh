1dd39e25b6b35e125491cd48ae3a97f87efbba77
Kurt Griffiths <kurt.griffiths@rackspace.com>
2015-02-18 11:53:52 -0600

    chore: Sign uploads to pypi

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #python setup.py bdist_egg upload
-python setup.py sdist --formats=gztar,zip upload
+python setup.py sdist --formats=gztar,zip upload --sign
 # python setup.py bdist_wheel upload
