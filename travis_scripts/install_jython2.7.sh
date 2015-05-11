57123f24d8161fc2283ab546cf7cf4fb93c359ec
Kurt Griffiths <kurt.griffiths@rackspace.com>
2015-05-11 14:35:44 -0500

    chore(Jython): Update Travis script to pull Jython 2.7.0 final

--- travis_scripts/install_jython2.7.sh
+++ travis_scripts/install_jython2.7.sh
@@ -1,3 +1,3 @@
-JYTHON_URL="http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-rc2/jython-installer-2.7-rc2.jar"
+JYTHON_URL="http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.0/jython-installer-2.7.0.jar"
 wget $JYTHON_URL -O jython_installer.jar
 java -jar jython_installer.jar -s -d $HOME/jython
