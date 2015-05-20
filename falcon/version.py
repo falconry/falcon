4e0ec585c54c15f9fd0608a6cbeb680207c14328
Kurt Griffiths <kurt.griffiths@rackspace.com>
2015-05-20 12:31:53 -0500

    chore: Open 0.4 development

--- falcon/version.py
+++ falcon/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 """Current version of Falcon."""
