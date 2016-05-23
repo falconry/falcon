92688f06c69db582d4106a0315c8b62f6c599b58
Kurt Griffiths <kurt.griffiths@rackspace.com>
2016-05-23 14:43:35 -0500

    Open 1.1.0 development

--- falcon/version.py
+++ falcon/version.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 """Current version of Falcon."""
