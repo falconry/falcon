ce3339ef4ab05345922fd59085e9e39193371a73
Kurt Griffiths <kurt.griffiths@rackspace.com>
2016-06-15 11:22:28 -0500

    chore(docs): Migrate to sphinx.ext.napolean

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
