2cd35bfefd5352cc5d104946f2763ef91f8041ee
Usman Ehtesham Gul <uehtesham90@gmail.com>
2016-06-21 11:39:15 -0400

    fix(pep8): fix pep8 errors detected by flak8 (F405 and F401) (#831)
    Fix pull requests not passing in Travis due to failure of ```tox -e pep8```. This is due to
    flake8 failing with F405 and F401 warning.
    
    Fixes #830

--- falcon/util/__init__.py
+++ falcon/util/__init__.py
@@ -1,6 +1,6 @@
 # Hoist misc. utils
 from falcon.util import structures
 from falcon.util.misc import *  # NOQA
-from falcon.util.time import *
+from falcon.util.time import *  # NOQA
 
 CaseInsensitiveDict = structures.CaseInsensitiveDict
