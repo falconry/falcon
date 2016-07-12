c80f859d00ec9d2e1acc2eeea433dc63a9bf197e
Lindsey Brockman <lbrockman@shiftgig.com>
2016-07-12 17:46:45 -0500

    fleshing out the rest of the default filters

--- falcon/routing/filters.py
+++ falcon/routing/filters.py
@@ -1,37 +1,78 @@
-from falcon.errors import HTTPBadRequest
+import re
+import uuid
+
+from falcon.errors import HTTPNotFound
 
 
 # TODO: should non-configurable filters be defined in such a way that they do not allow the second param? Or
 # should it just be ignored?
-def int_filter(segments, filter_config=None):
+def int_filter(segments, config=None):
     try:
         value = int(segments[0])
     except ValueError:
-        raise HTTPBadRequest
+        raise HTTPNotFound()
     else:
         filter_matched = True
         filter_consumed_all_segments = False
         return (filter_matched, filter_consumed_all_segments, value)
 
 
-def float_filter(segments, filter_config=None):
+def float_filter(segments, config=None):
     try:
         value = float(segments[0])
     except ValueError:
-        raise HTTPBadRequest
+        raise HTTPNotFound()
     else:
         filter_matched = True
         filter_consumed_all_segments = False
         return (filter_matched, filter_consumed_all_segments, value)
 
 
-def path_filter(segments, filter_config=None):
-    pass
+def path_filter(segments, config=None):
+    config = '^.+'
+    re_config = re.compile(config)
+    remaining_url = '/'.join(segments)
+
+    first_segment_match = re_config.match(segments[0])
+    all_segments_match = re_config.match(remaining_url)
+
+    try:
+        assert first_segment_match or all_segments_match
+    except AssertionError:
+        raise HTTPNotFound()
+    else:
+        value = all_segments_match or first_segment_match
+        filter_matched = True
+        filter_consumed_all_segments = bool(all_segments_match)
+        return (filter_matched, filter_consumed_all_segments, value)
 
 
-def uuid_filter(segments, filter_config=None):
-    pass
+def re_filter(segments, config=None):
+    re_config = re.compile(config)
+    remaining_url = '/'.join(segments)
+
+    first_segment_match = re_config.match(segments[0])
+    all_segments_match = re_config.match(remaining_url)
+
+    try:
+        assert first_segment_match or all_segments_match
+    except AssertionError:
+        raise HTTPNotFound()
+    else:
+        value = all_segments_match or first_segment_match
+        filter_matched = True
+        filter_consumed_all_segments = bool(all_segments_match)
+        return (filter_matched, filter_consumed_all_segments, value)
 
 
-def re_filter(segments, filter_config=None):
-    pass
+def uuid_filter(segments, config=None):
+    try:
+        value = uuid.UUID(segments[0], version=4)
+    except ValueError:
+        raise HTTPNotFound()
+    else:
+        filter_matched = True
+        filter_consumed_all_segments = False
+        return (filter_matched, filter_consumed_all_segments, value)
+
+
