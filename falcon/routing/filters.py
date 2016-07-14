c7c14e3279e3135499dbeae87fc5361c75578d48
Lindsey Brockman <lbrockman@shiftgig.com>
2016-07-13 19:52:18 -0500

    created a registry class

--- falcon/routing/filters.py
+++ falcon/routing/filters.py
@@ -4,75 +4,63 @@ import uuid
 from falcon.errors import HTTPNotFound
 
 
-# TODO: should non-configurable filters be defined in such a way that they do not allow the second param? Or
-# should it just be ignored?
-def int_filter(segments, config=None):
-    try:
-        value = int(segments[0])
-    except ValueError:
-        raise HTTPNotFound()
-    else:
-        filter_matched = True
-        filter_consumed_all_segments = False
-        return (filter_matched, filter_consumed_all_segments, value)
+class FilterRegistry(object):
+    def __init__(self):
+        self.registry = {}
 
+        self.register_filter('int', int_filter)
+        self.register_filter('float', float_filter)
+        self.register_filter('path', path_filter)
+        self.register_filter('re', re_filter)
+        self.register_filter('uuid', uuid_filter)
+
+    def __getitem__(self, filter_name):
+        return self.registry.__getitem__(filter_name)
+
+    def register_filter(self, name, func):
+        self.registry[name] = self._wrap_filter(func)
+
+    @staticmethod
+    def _wrap_filter(filter_function):
+        def wrapped_filter(segments, config=None):
+            try:
+                num_segments_matched, value = filter_function(segments, config)
+                # NOTE(lindseybrockman): Currently only filters that match either the
+                # first filtered segment or all the remaining segments are supported
+                assert num_segments_matched == 1 or num_segments_matched == len(segments)
+            except:
+                raise HTTPNotFound()
+            else:
+                return (num_segments_matched, value)
+
+        return wrapped_filter
+
+def int_filter(segments, config=None):
+    return 1, int(segments[0])
 
 def float_filter(segments, config=None):
-    try:
-        value = float(segments[0])
-    except ValueError:
-        raise HTTPNotFound()
-    else:
-        filter_matched = True
-        filter_consumed_all_segments = False
-        return (filter_matched, filter_consumed_all_segments, value)
-
+    return 1, float(segments[0])
 
 def path_filter(segments, config=None):
-    config = '^.+'
-    re_config = re.compile(config)
-    remaining_url = '/'.join(segments)
+    config='(^.+)'
 
-    first_segment_match = re_config.match(segments[0])
-    all_segments_match = re_config.match(remaining_url)
-
-    try:
-        assert first_segment_match or all_segments_match
-    except AssertionError:
-        raise HTTPNotFound()
-    else:
-        value = all_segments_match or first_segment_match
-        filter_matched = True
-        filter_consumed_all_segments = bool(all_segments_match)
-        return (filter_matched, filter_consumed_all_segments, value)
-
+    return re_filter(segments, config)
 
 def re_filter(segments, config=None):
+    if not config.startswith('(') and not config.endswith(')'):
+        config = '({})'.format(config)
+
     re_config = re.compile(config)
     remaining_url = '/'.join(segments)
 
-    first_segment_match = re_config.match(segments[0])
-    all_segments_match = re_config.match(remaining_url)
+    match = re_config.match(remaining_url)
 
-    try:
-        assert first_segment_match or all_segments_match
-    except AssertionError:
-        raise HTTPNotFound()
-    else:
-        value = all_segments_match or first_segment_match
-        filter_matched = True
-        filter_consumed_all_segments = bool(all_segments_match)
-        return (filter_matched, filter_consumed_all_segments, value)
+    if match.group() == segments[0]:
+        num_segments_matched = 1
+    elif match.group() == remaining_url:
+        num_segments_matched = len(segments)
 
+    return num_segments_matched, match.groups()
 
 def uuid_filter(segments, config=None):
-    try:
-        value = uuid.UUID(segments[0], version=4)
-    except ValueError:
-        raise HTTPNotFound()
-    else:
-        filter_matched = True
-        filter_consumed_all_segments = False
-        return (filter_matched, filter_consumed_all_segments, value)
-
-
+    return 1, uuid.UUID(segments[0], version=4)
