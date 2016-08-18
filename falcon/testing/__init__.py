fb7ca62feaa617c868fef4a52cacda736d75b99d
Kurt Griffiths <kgriffs@users.noreply.github.com>
2016-08-17 22:22:15 -0500

    feat(testing): Add TestClient class and a pytest example (#862)
    * feat(testing): Add TestClient class and a pytest example
    
    Add a contextual wrapper for the simulate_* methods and a pytest
    example demonstrating its use.
    
    Also deprecate "api" in favor of "app" in TestCase, since these utils
    can work with any WSGI app (not just those that express HTTP APIs). This
    terminology makes TestCase more consistent with the new simulate_*
    functions and with TestClient.
    
    Finally, deprecate "api_class" in TestCase and update the docstring
    for the testing module to demonstrate a less hacky alternative for
    controlling the api instance type. As a bonus, this avoids having to
    try and alias api_class (a class variable).
    
    Partially implements #16
    
    * doc(testing): Clarify 'myapp' example code

--- falcon/testing/__init__.py
+++ falcon/testing/__init__.py
@@ -16,23 +16,58 @@
 
 This package contains various test classes and utility functions to
 support functional testing for both Falcon-based apps and the Falcon
-framework itself::
+framework itself. Both unittest-style and pytest-style tests are
+supported::
+
+    # -----------------------------------------------------------------
+    # unittest-style
+    # -----------------------------------------------------------------
 
     from falcon import testing
-    from myapp import app
+    import myapp
 
-    class TestMyApp(testing.TestCase):
+
+    class MyTestCase(testing.TestCase):
         def setUp(self):
-            super(TestMyApp, self).setUp()
-            self.api = app.create_api()
+            super(MyTestCase, self).setUp()
 
-    def test_get_message(self):
+            # Assume the hypothetical `myapp` package has a
+            # function called `create()` to initialize and
+            # return a `falcon.API` instance.
+            self.app = myapp.create()
+
+
+    class TestMyApp(MyTestCase):
+        def test_get_message(self):
+            doc = {u'message': u'Hello world!'}
+
+            result = self.simulate_get('/messages/42')
+            self.assertEqual(result.json, doc)
+
+
+    # -----------------------------------------------------------------
+    # pytest-style
+    # -----------------------------------------------------------------
+
+    from falcon import testing
+    import pytest
+
+    import myapp
+
+
+    @pytest.fixture(scope='module')
+    def client():
+        # Assume the hypothetical `myapp` package has a
+        # function called `create()` to initialize and
+        # return a `falcon.API` instance.
+        return testing.TestClient(myapp.create())
+
+
+    def test_get_message(client):
         doc = {u'message': u'Hello world!'}
 
-        result = self.simulate_get('/messages/42')
-        self.assertEqual(result.json, doc)
-
-For additional examples, see also Falcon's own test suite.
+        result = client.simulate_get('/messages/42')
+        assert result.json == doc
 """
 
 # Hoist classes and functions into the falcon.testing namespace
