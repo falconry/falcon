145f4bef9a5c9617659ee132cb26aa329ee51ae4
Kurt Griffiths <kgriffs@users.noreply.github.com>
2016-10-29 10:15:49 -0600

    test: Make dump_wsgi.py work on both py2 and py3k (#935)

--- tests/dump_wsgi.py
+++ tests/dump_wsgi.py
@@ -11,16 +11,18 @@ def application(environ, start_response):
 
     body += '}\n\n'
 
-    return [body.encode('utf-8')]
+    if not isinstance(body, bytes):
+        body = body.encode('utf-8')
+
+    return [body]
 
 app = application
 
 
 if __name__ == '__main__':
-    # import eventlet.wsgi
-    # import eventlet
-    # eventlet.wsgi.server(eventlet.listen(('localhost', 8000)), application)
-
     from wsgiref.simple_server import make_server
     server = make_server('localhost', 8000, application)
+
+    print('Listening on localhost:8000...')
+
     server.serve_forever()
