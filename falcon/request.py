be8dbe92b64361bf2c77cb5c34e7c0703eb2cd01
Kurt Griffiths <kurt.griffiths@rackspace.com>
2016-11-04 15:14:43 -0600

    doc(Request): Add missing colon after "Keyword Arguments"

--- falcon/request.py
+++ falcon/request.py
@@ -65,7 +65,7 @@ class Request(object):
         env (dict): A WSGI environment dict passed in from the server. See
             also PEP-3333.
 
-    Keyword Arguments
+    Keyword Arguments:
         options (dict): Set of global options passed from the API handler.
 
     Attributes:
