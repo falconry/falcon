0b4cfb112b74cc6fb7684b2dd6005e3fc61d5d6a
Kurt Griffiths <kurt.griffiths@rackspace.com>
2017-01-27 14:02:48 -0700

    doc(HTTPFailedDependency): Remove PROPPATCH reference

--- falcon/errors.py
+++ falcon/errors.py
@@ -925,9 +925,7 @@ class HTTPFailedDependency(OptionalRepresentation, HTTPError):
 
     The 424 (Failed Dependency) status code means that the method could
     not be performed on the resource because the requested action
-    depended on another action and that action failed. For example, if a
-    command in a PROPPATCH method fails, then, at minimum, the rest of
-    the commands will also fail with 424 (Failed Dependency).
+    depended on another action and that action failed.
 
     (See also: RFC 4918, Section 11.4)
 
