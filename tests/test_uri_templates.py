e1852a2d5121a0d3d2ee56793ef0803cba3f6620
Kurt Griffiths <kurt.griffiths@rackspace.com>
2017-04-10 17:26:52 -0600

    test: Migrate URI template and router testing to pytest
    Note that some tests were moved from test_uri_templates to
    test_default_router.

--- tests/test_uri_templates.py
+++ tests/test_uri_templates.py
@@ -1,8 +1,15 @@
-import ddt
+"""Application tests for URI templates using simulate_get().
+
+These tests differ from those in test_default_router in that they are
+a collection of sanity-checks that exercise the full framework code
+path via simulate_get(), vs. probing the router directly.
+"""
+
+import pytest
 import six
 
 import falcon
-import falcon.testing as testing
+from falcon import testing
 
 
 class IDResource(object):
@@ -63,204 +70,154 @@ class FileDetailsResource(object):
         self.called = True
 
 
-@ddt.ddt
-class TestUriTemplates(testing.TestBase):
+@pytest.fixture
+def resource():
+    return testing.SimpleTestResource()
 
-    def before(self):
-        self.resource = testing.TestResource()
 
-    def test_root_path(self):
-        self.api.add_route('/', self.resource)
-        self.simulate_request('/')
+@pytest.fixture
+def client():
+    return testing.TestClient(falcon.API())
 
-        self.assertTrue(self.resource.called)
-        req = self.resource.req
 
-        self.assertEqual(req.get_param('id'), None)
+def test_root_path(client, resource):
+    client.app.add_route('/', resource)
+    client.simulate_get('/')
+    assert resource.called
 
-    def test_not_str(self):
-        self.assertRaises(TypeError, self.api.add_route, {}, self.resource)
-        self.assertRaises(TypeError, self.api.add_route, [], self.resource)
-        self.assertRaises(TypeError, self.api.add_route, set(), self.resource)
-        self.assertRaises(TypeError, self.api.add_route, self, self.resource)
 
-    @ddt.data('/hello/{1world}', '/{524hello}/world')
-    def test_field_name_cannot_start_with_digit(self, route):
-        self.assertRaises(ValueError, self.api.add_route, route, self.resource)
+def test_no_vars(client, resource):
+    client.app.add_route('/hello/world', resource)
+    client.simulate_get('/hello/world')
+    assert resource.called
 
-    @ddt.data(
-        '/{thing }/world',
-        '/{ thing}/world',
-        '/{ thing }/world',
-        '/{thing}/wo rld',
-        '/{thing} /world'
-    )
-    def test_whitespace_not_allowed(self, route):
-        self.assertRaises(ValueError, self.api.add_route, route, self.resource)
 
-    def test_no_vars(self):
-        self.api.add_route('/hello/world', self.resource)
-        self.simulate_request('/hello/world')
+@pytest.mark.skipif(six.PY3, reason='Test only applies to Python 2')
+def test_unicode_literal_routes(client, resource):
+    client.app.add_route(u'/hello/world', resource)
+    client.simulate_get('/hello/world')
+    assert resource.called
 
-        self.assertTrue(self.resource.called)
-        req = self.resource.req
 
-        self.assertEqual(req.get_param('world'), None)
+def test_special_chars(client, resource):
+    client.app.add_route('/hello/world.json', resource)
+    client.app.add_route('/hello(world)', resource)
 
-    def test_unicode_literal_routes(self):
-        if six.PY3:
-            self.skipTest('Test only applies to Python 2')
+    client.simulate_get('/hello/world_json')
+    assert not resource.called
 
-        self.api.add_route(u'/hello/world', self.resource)
-        self.simulate_request('/hello/world')
+    client.simulate_get('/helloworld')
+    assert not resource.called
 
-        self.assertTrue(self.resource.called)
-        req = self.resource.req
+    client.simulate_get('/hello/world.json')
+    assert resource.called
 
-        self.assertEqual(req.get_param('world'), None)
+    client.simulate_get('/hello(world)')
+    assert resource.called
 
-    def test_special_chars(self):
-        self.api.add_route('/hello/world.json', self.resource)
-        self.api.add_route('/hello(world)', self.resource)
 
-        self.simulate_request('/hello/world_json')
-        self.assertFalse(self.resource.called)
+@pytest.mark.parametrize('field_name', [
+    'id',
+    'id123',
+    'widget_id',
+])
+def test_single(client, resource, field_name):
+    template = '/widgets/{{{0}}}'.format(field_name)
 
-        self.simulate_request('/helloworld')
-        self.assertFalse(self.resource.called)
+    client.app.add_route(template, resource)
 
-        self.simulate_request('/hello/world.json')
-        self.assertTrue(self.resource.called)
+    client.simulate_get('/widgets/123')
+    assert resource.called
+    assert resource.captured_kwargs[field_name] == '123'
 
-    def test_single(self):
-        self.api.add_route('/widgets/{id}', self.resource)
 
-        self.simulate_request('/widgets/123')
-        self.assertTrue(self.resource.called)
+def test_single_trailing_slash(client):
+    resource1 = IDResource()
+    client.app.add_route('/1/{id}/', resource1)
+    result = client.simulate_get('/1/123')
+    assert result.status == falcon.HTTP_200
+    assert resource1.called
+    assert resource1.id == '123'
+    assert resource1.req.path == '/1/123'
 
-        req = self.resource.req
-        kwargs = self.resource.kwargs
-        self.assertEqual(kwargs['id'], '123')
-        self.assertNotIn(kwargs, 'Id')
-        self.assertEqual(req.get_param('id'), None)
+    resource2 = IDResource()
+    client.app.add_route('/2/{id}/', resource2)
+    result = client.simulate_get('/2/123/')
+    assert result.status == falcon.HTTP_200
+    assert resource2.called
+    assert resource2.id == '123'
+    assert resource2.req.path == '/2/123'
 
-    def test_single_with_trailing_digits(self):
-        self.api.add_route('/widgets/{id12}', self.resource)
+    resource3 = IDResource()
+    client.app.add_route('/3/{id}', resource3)
+    result = client.simulate_get('/3/123/')
+    assert result.status == falcon.HTTP_200
+    assert resource3.called
+    assert resource3.id == '123'
+    assert resource3.req.path == '/3/123'
 
-        self.simulate_request('/widgets/123')
-        self.assertTrue(self.resource.called)
-        self.assertEqual(self.resource.kwargs['id12'], '123')
 
-    def test_single_with_underscore(self):
-        self.api.add_route('/widgets/{widget_id}', self.resource)
+def test_multiple(client):
+    resource = NameResource()
+    client.app.add_route('/messages/{id}/names/{name}', resource)
 
-        self.simulate_request('/widgets/123')
-        self.assertTrue(self.resource.called)
-        self.assertEqual(self.resource.kwargs['widget_id'], '123')
+    test_id = 'bfb54d43-219b-4336-a623-6172f920592e'
+    test_name = '758e3922-dd6d-4007-a589-50fba0789365'
+    path = '/messages/' + test_id + '/names/' + test_name
+    client.simulate_get(path)
 
-    def test_single_trailing_slash(self):
-        resource1 = IDResource()
-        self.api.add_route('/1/{id}/', resource1)
+    assert resource.called
+    assert resource.id == test_id
+    assert resource.name == test_name
 
-        self.simulate_request('/1/123')
-        self.assertEqual(self.srmock.status, falcon.HTTP_200)
-        self.assertTrue(resource1.called)
-        self.assertEqual(resource1.id, '123')
-        self.assertEqual(resource1.name, None)
-        self.assertEqual(resource1.req.path, '/1/123')
 
-        resource2 = IDResource()
-        self.api.add_route('/2/{id}/', resource2)
+@pytest.mark.parametrize('uri_template', [
+    '//',
+    '//begin',
+    '/end//',
+    '/in//side',
+])
+def test_empty_path_component(client, resource, uri_template):
+    with pytest.raises(ValueError):
+        client.app.add_route(uri_template, resource)
 
-        self.simulate_request('/2/123/')
-        self.assertTrue(resource2.called)
-        self.assertEqual(resource2.id, '123')
-        self.assertEqual(resource2.name, None)
-        self.assertEqual(resource2.req.path, '/2/123')
 
-        resource3 = IDResource()
-        self.api.add_route('/3/{id}', resource3)
+@pytest.mark.parametrize('uri_template', [
+    '',
+    'no',
+    'no/leading_slash',
+])
+def test_relative_path(client, resource, uri_template):
+    with pytest.raises(ValueError):
+        client.app.add_route(uri_template, resource)
 
-        self.simulate_request('/3/123/')
-        self.assertTrue(resource3.called)
-        self.assertEqual(resource3.id, '123')
-        self.assertEqual(resource3.name, None)
-        self.assertEqual(resource3.req.path, '/3/123')
 
-    def test_multiple(self):
-        resource = NameResource()
-        self.api.add_route('/messages/{id}/names/{name}', resource)
+@pytest.mark.parametrize('reverse', [True, False])
+def test_same_level_complex_var(client, reverse):
+    file_resource = FileResource()
+    details_resource = FileDetailsResource()
 
-        test_id = self.getUniqueString()
-        test_name = self.getUniqueString()
-        path = '/messages/' + test_id + '/names/' + test_name
-        self.simulate_request(path)
-        self.assertTrue(resource.called)
+    routes = [
+        ('/files/{file_id}', file_resource),
+        ('/files/{file_id}.{ext}', details_resource)
+    ]
+    if reverse:
+        routes.reverse()
 
-        self.assertEqual(resource.id, test_id)
-        self.assertEqual(resource.name, test_name)
+    for uri_template, resource in routes:
+        client.app.add_route(uri_template, resource)
 
-    def test_multiple_with_digits(self):
-        resource = NameAndDigitResource()
-        self.api.add_route('/messages/{id}/names/{name51}', resource)
+    file_id_1 = 'bc6b201d-b449-4290-a061-8eeb9f7b1450'
+    file_id_2 = '33b7f34c-6ee6-40e6-89a3-742a69b59de0'
+    ext = 'a4581b95-bc36-4c08-a3c2-23ba266abdf2'
+    path_1 = '/files/' + file_id_1
+    path_2 = '/files/' + file_id_2 + '.' + ext
 
-        test_id = self.getUniqueString()
-        test_name = self.getUniqueString()
-        path = '/messages/' + test_id + '/names/' + test_name
-        self.simulate_request(path)
-        self.assertTrue(resource.called)
+    client.simulate_get(path_1)
+    assert file_resource.called
+    assert file_resource.file_id == file_id_1
 
-        self.assertEqual(resource.id, test_id)
-        self.assertEqual(resource.name51, test_name)
-
-    @ddt.data('//', '//begin', '/end//', '/in//side')
-    def test_empty_path_component(self, route):
-        self.assertRaises(ValueError, self.api.add_route, route, self.resource)
-
-    @ddt.data('', 'no', 'no/leading_slash')
-    def test_relative_path(self, route):
-        self.assertRaises(ValueError, self.api.add_route, route, self.resource)
-
-    def test_same_level_complex_var(self):
-        resource = FileResource()
-        details_resource = FileDetailsResource()
-        self.api.add_route('/files/{file_id}', resource)
-        self.api.add_route('/files/{file_id}.{ext}', details_resource)
-
-        # dots cause ambiguous in filenames
-        file_id_1 = self.getUniqueString().replace('.', '-')
-        file_id_2 = self.getUniqueString().replace('.', '-')
-        ext = self.getUniqueString().replace('.', '-')
-        path_1 = '/files/' + file_id_1
-        path_2 = '/files/' + file_id_2 + '.' + ext
-
-        self.simulate_request(path_1)
-        self.assertTrue(resource.called)
-        self.assertEqual(resource.file_id, file_id_1)
-
-        self.simulate_request(path_2)
-        self.assertTrue(details_resource.called)
-        self.assertEqual(details_resource.file_id, file_id_2)
-        self.assertEqual(details_resource.ext, ext)
-
-    def test_same_level_complex_var_in_reverse_order(self):
-        resource = FileResource()
-        details_resource = FileDetailsResource()
-        self.api.add_route('/files/{file_id}.{ext}', details_resource)
-        self.api.add_route('/files/{file_id}', resource)
-
-        # dots cause ambiguous in filenames
-        file_id_1 = self.getUniqueString().replace('.', '-')
-        file_id_2 = self.getUniqueString().replace('.', '-')
-        ext = self.getUniqueString().replace('.', '-')
-        path_1 = '/files/' + file_id_1
-        path_2 = '/files/' + file_id_2 + '.' + ext
-
-        self.simulate_request(path_1)
-        self.assertTrue(resource.called)
-        self.assertEqual(resource.file_id, file_id_1)
-
-        self.simulate_request(path_2)
-        self.assertTrue(details_resource.called)
-        self.assertEqual(details_resource.file_id, file_id_2)
-        self.assertEqual(details_resource.ext, ext)
+    client.simulate_get(path_2)
+    assert details_resource.called
+    assert details_resource.file_id == file_id_2
+    assert details_resource.ext == ext
