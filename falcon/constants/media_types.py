5d92bde87a83791ce4a173b36a1e1f540d05d71f
Mayur Mahajan <mayurm@zillowgroup.com>
2017-05-23 10:53:58 -0700

    correcting xml mime type

--- falcon/constants/media_types.py
+++ falcon/constants/media_types.py
@@ -1,7 +1,7 @@
 MEDIA_JSON = 'application/json; charset=UTF-8'
 MEDIA_HTML = 'text/html; charset=utf-8'
 MEDIA_JS = 'text/javascript; charset=utf-8'
-MEDIA_XML = 'text/javascript'
+MEDIA_XML = 'application/xml'
 MEDIA_TEXT = 'text/plain; charset=utf-8'
 MEDIA_JPEG = 'image/jpeg'
 MEDIA_PNG = 'image/png'
