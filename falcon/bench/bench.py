abc2c175571a4ff801d4db75feb977371e064416
Kurt Griffiths <kurt.griffiths@rackspace.com>
2017-07-19 14:12:14 -0600

    test(bench): Add Django benchmark

--- falcon/bench/bench.py
+++ falcon/bench/bench.py
@@ -254,11 +254,12 @@ def run(frameworks, trials, iterations, stat_memory):
 def main():
     frameworks = [
         'bottle',
+        'django',
         'falcon',
         'falcon-ext',
         'flask',
         'pecan',
-        'werkzeug'
+        'werkzeug',
     ]
 
     parser = argparse.ArgumentParser(description='Falcon benchmark runner')
@@ -313,7 +314,7 @@ def main():
         us_per_req = (sec_per_req * Decimal(10 ** 6))
         factor = round_to_int(baseline / sec_per_req)
 
-        print('{3}. {0:.<15s}{1:.>06d} req/sec or {2: >3.2f} μs/req ({4}x)'.
+        print('{3}. {0:.<20s}{1:.>06d} req/sec or {2: >3.2f} μs/req ({4}x)'.
               format(name, req_per_sec, us_per_req, i + 1, factor))
 
     if heapy and args.stat_memory:
---
+++ falcon/bench/dj/dj/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from hello import views
+
+urlpatterns = [
+    url(r'^hello/(?P<account_id>[0-9]+)/test$', views.hello)
+]
