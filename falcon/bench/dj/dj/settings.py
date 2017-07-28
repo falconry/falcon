478a3419bd2aaea6d1036aee5e182a3d2624ef31
John Vrbanac <john.vrbanac@linux.com>
2017-07-28 14:34:21 -0500

    chore(bench): Generate django secret on-demand (#1088)
    A community member (@AndrewBurdyug) mentioned this as a cleaner
    solution.

--- falcon/bench/dj/dj/settings.py
+++ falcon/bench/dj/dj/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from django.core.management import utils
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't6gj%z0ee)*xmdoqhm4^er60=s^1g_vs7y!dl2vve4-1u9^+ps'
+SECRET_KEY = utils.get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
