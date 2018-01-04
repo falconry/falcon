ab6d38e666cdd3d22162e2dfa07700891218392b
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2018-01-05 00:47:41 +0100

    fix(media): Fix failing response media serialization on py27+ujson (#1191)
    Fix falcon.media.JSONHandler raising an error under Python 2.x 
    when serializing strings containing Unicode code points.

--- tests/test_response_media.py
+++ tests/test_response_media.py
@@ -21,6 +21,18 @@ def create_client(handlers=None):
     return client
 
 
+class SimpleMediaResource(object):
+
+    def __init__(self, document, media_type=falcon.MEDIA_JSON):
+        self._document = document
+        self._media_type = media_type
+
+    def on_get(self, req, resp):
+        resp.content_type = self._media_type
+        resp.media = self._document
+        resp.status = falcon.HTTP_OK
+
+
 @pytest.mark.parametrize('media_type', [
     ('*/*'),
     (falcon.MEDIA_JSON),
@@ -35,6 +47,32 @@ def test_json(media_type):
     resp.media = {'something': True}
 
     assert json.loads(resp.data.decode('utf-8')) == {u'something': True}
+
+
+@pytest.mark.parametrize('document', [
+    '',
+    u'I am a \u1d0a\ua731\u1d0f\u0274 string.',
+    [u'\u2665', u'\u2660', u'\u2666', u'\u2663'],
+    {u'message': u'\xa1Hello Unicode! \U0001F638'},
+    {
+        'description': 'A collection of primitive Python 2 type examples.',
+        'bool': False is not True and True is not False,
+        'dict': {'example': 'mapping'},
+        'float': 1.0,
+        'int': 1337,
+        'list': ['a', 'sequence', 'of', 'items'],
+        'none': None,
+        'str': 'ASCII string',
+        'unicode': u'Hello Unicode! \U0001F638',
+    },
+])
+def test_non_ascii_json_serialization(document):
+    app = falcon.API()
+    app.add_route('/', SimpleMediaResource(document))
+    client = testing.TestClient(app)
+
+    resp = client.simulate_get('/')
+    assert resp.json == document
 
 
 @pytest.mark.parametrize('media_type', [
