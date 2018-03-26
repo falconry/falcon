fb2c12cbfc9a5ee902d9614e6aa9461d1431527c
branelmoro <branelmoro@gmail.com>
2018-03-27 00:20:05 +0530

    file upload testcase added

--- tests/test_multipart_url_encoded.py
+++ tests/test_multipart_url_encoded.py
@@ -1,11 +1,9 @@
-import json
 import os
-import pytest
 import requests
+import tempfile
 
 from falcon.request import Request
 import falcon.testing as testing
-
 
 def test_urlencoded():
     """Ensure that urlencoded input format works as intended"""
@@ -17,82 +15,30 @@ def test_urlencoded():
         app='backoffice',
         headers=headers,
         body=test_data,
-        method="POST"
+        method='POST'
     ))
-
     assert req.form_data == {'name': 'John Doe', 'foo': ['baz', 'bar']}
-
 
 def test_multipart_file_upload():
     """Ensure that multipart input format works as intended"""
-    # @hug.post()
-    # def test_multipart_post(**kwargs):
-    #     return kwargs
-
-    # with open(os.path.join(BASE_DIRECTORY, 'artwork', 'logo.png'),'rb') as logo:
-    #     prepared_request = requests.Request('POST', 'http://localhost/', files={'logo': logo}).prepare()
-    #     logo.seek(0)
-    #     output = json.loads(hug.defaults.output_format({'logo': logo.read()}).decode('utf8'))
-    #     assert hug.test.post(api, 'test_multipart_post',  body=prepared_request.body,
-    #                          headers=prepared_request.headers).data == output
-
-
-
-    # import sys
-    # import traceback
-    # import os
-
-    # import json
-    # import datetime
-    # import time
-
-    # import fetcher_tables
-
-    # import pymysql
-
-    # import elasticsearch
-    # from redis_crud import redisCrud
-
-    # import threading
-
-    # from pathlib import Path
-
-    # import config
-
-    # import data_source.dataGenerator as DATA_GENERATOR
-
-    PATH_TO_UPLOAD_FILE = os.path.dirname(os.path.abspath(__file__)) + "/../logo/banner.jpg"
-
+    PATH_TO_UPLOAD_FILE = os.path.dirname(os.path.abspath(__file__)) + '/../logo/banner.jpg'
     with open(PATH_TO_UPLOAD_FILE,'rb') as logo:
-
         prepared_request = requests.Request('POST', 'http://localhost/', files={'logo': logo}).prepare()
         logo.seek(0)
-
-        # output = json.loads(hug.defaults.output_format({'logo': logo.read()}).decode('utf8'))
-
         output = logo.read()
-
         req = Request(testing.create_environ(
             host='example.com',
             path='/languages',
             app='backoffice',
             headers=prepared_request.headers,
             body=prepared_request.body,
-            method="POST"
+            method='POST'
         ))
-
-        # assert req.files == {'name': 'John Doe', 'foo': ['baz', 'bar']}
-
-
-        req.files["pancard"].set_max_upload_size(1*1024*1024)
-
-        path = "/tmp/" + req.files["pancard"].name
-
-        response = req.files["pancard"].uploadto("/tmp/" + path)
-
-        
-        assert req.files == output
-
-
-        
-        # assert hug.test.post(api, 'test_multipart_post',  body=prepared_request.body, headers=prepared_request.headers).data == output
+        req.files['logo'].set_max_upload_size(1*1024*1024)
+        file_path = tempfile.gettempdir() + '/' + req.files['logo'].name
+        upload_status = req.files['logo'].uploadto(file_path)
+        assert upload_status == True
+        with open(file_path,'rb') as uploaded_file:
+            content = uploaded_file.read()
+            assert content == output
+            os.remove(file_path)
