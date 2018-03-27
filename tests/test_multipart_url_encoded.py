f55a82de3a607ae88e1ceb75256540ea9dbe61f3
Branel Moro <PEOPLE\branelm@lp7782.people.local>
2018-03-27 16:17:06 +0530

    code coverage issue resolved

--- tests/test_multipart_url_encoded.py
+++ tests/test_multipart_url_encoded.py
@@ -40,67 +40,70 @@ def test_multipart_file_upload():
     file3_path = os.path.dirname(os.path.abspath(__file__)) + '/../logo/logo.svg'
     file4_path = os.path.dirname(os.path.abspath(__file__)) + '/../logo/mstile-150x150.png'
     file5_path = os.path.dirname(os.path.abspath(__file__)) + '/../logo/mstile-310x310.png'
-    with open(file1_path, 'rb') as file1,\
-    open(file2_path, 'rb') as file2,\
-    open(file3_path, 'rb') as file3,\
-    open(file4_path, 'rb') as file4,\
-    open(file5_path, 'rb') as file5:
 
-        text_fields = {
-            'name': 'John Doe',
-            'foo': ['baz', 'bar'],
-            'file1': 'John Doe',
-            'file2': ['baz', 'bar'],
-            'multifile': ['baz', 'bar'],
-            'blank_field': ''
-        }
-        file_fields = {
-            'file1': file1,
-            'file2': file2,
-            'multifile': file3,
-            'file4': file4,
-            'file5': file5,
-            'enptyFileField': b''
-        }
-        prepared_request = requests.Request(
-            method='POST',
-            url='http://localhost/',
-            data=text_fields,
-            files=file_fields
-        ).prepare()
+    file1 = open(file1_path, 'rb')
+    file2 = open(file2_path, 'rb')
+    file3 = open(file3_path, 'rb')
+    file4 = open(file4_path, 'rb')
+    file5 = open(file5_path, 'rb')
 
-        file1.seek(0)
-        file1_content = file1.read()
+    text_fields = {
+        'name': 'John Doe',
+        'foo': ['baz', 'bar'],
+        'file1': 'John Doe',
+        'file2': ['baz', 'bar'],
+        'multifile': ['baz', 'bar'],
+        'blank_field': ''
+    }
+    file_fields = {
+        'file1': file1,
+        'file2': file2,
+        'multifile': file3,
+        'file4': file4,
+        'file5': file5,
+        'enptyFileField': b''
+    }
+    prepared_request = requests.Request(
+        method='POST',
+        url='http://localhost/',
+        data=text_fields,
+        files=file_fields
+    ).prepare()
 
-        file2.seek(0)
-        file2_content = file2.read()
+    file1.seek(0)
+    file1_content = file1.read()
 
-        file3.seek(0)
-        file3_content = file3.read()
+    file2.seek(0)
+    file2_content = file2.read()
 
-        file4.seek(0)
-        file4_content = file4.read()
+    file3.seek(0)
+    file3_content = file3.read()
 
-        file5.seek(0)
-        file5_content = file5.read()
+    file4.seek(0)
+    file4_content = file4.read()
 
-        req = Request(testing.create_environ(
-            host='example.com',
-            path='/languages',
-            app='backoffice',
-            headers=prepared_request.headers,
-            body=prepared_request.body,
-            method='POST'
-        ))
+    file5.seek(0)
+    file5_content = file5.read()
 
-        assert req.form_data == text_fields
+    req = Request(testing.create_environ(
+        host='example.com',
+        path='/languages',
+        app='backoffice',
+        headers=prepared_request.headers,
+        body=prepared_request.body,
+        method='POST'
+    ))
 
-        assertFileUpload(req.files['file1'], file1_content)
+    assert req.form_data == text_fields
+    
+    assertFileUpload(req.files['file1'], file1_content)
+    assertFileUpload(req.files['file2'], file2_content)
+    assertFileUpload(req.files['multifile'], file3_content)
+    assertFileUpload(req.files['file4'], file4_content)
+    assertFileUpload(req.files['file5'], file5_content)
 
-        assertFileUpload(req.files['file2'], file2_content)
-
-        assertFileUpload(req.files['multifile'], file3_content)
-
-        assertFileUpload(req.files['file4'], file4_content)
-
-        assertFileUpload(req.files['file5'], file5_content)
+    file1.close()
+    file2.close()
+    file3.close()
+    file4.close()
+    file5.close()
