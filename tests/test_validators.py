7d4ef037b3eae756fb5354850a7efa19bcf730d2
Kurt Griffiths <mail@kgriffs.com>
2018-04-23 09:23:07 -0600

    test(validators): Improve tests for validators.jsonschema

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -6,6 +6,7 @@ except ImportError:
 import pytest
 
 import falcon
+from falcon import testing
 from falcon.media import validators
 
 basic_schema = {
@@ -33,8 +34,19 @@ class Resource(object):
 
     @validators.jsonschema.validate(resp_schema=basic_schema)
     def response_validated(self, req, resp):
-        assert req.media is not None
+        assert resp.media is not None
         return resp
+
+    @validators.jsonschema.validate(req_schema=basic_schema, resp_schema=basic_schema)
+    def both_validated(self, req, resp):
+        assert req.media is not None
+        assert resp.media is not None
+        return req, resp
+
+    @validators.jsonschema.validate(req_schema=basic_schema, resp_schema=basic_schema)
+    def on_put(self, req, resp):
+        assert req.media is not None
+        resp.media = GoodData.media
 
 
 class GoodData(object):
@@ -53,9 +65,10 @@ def test_req_schema_validation_success():
 
 @skip_missing_dep
 def test_req_schema_validation_failure():
-    with pytest.raises(falcon.HTTPBadRequest) as err:
+    with pytest.raises(falcon.HTTPBadRequest) as excinfo:
         Resource().request_validated(BadData(), None)
-        assert err.value.description == '\'message\' is a required property'
+
+    assert excinfo.value.description == '\'message\' is a required property'
 
 
 @skip_missing_dep
@@ -66,6 +79,41 @@ def test_resp_schema_validation_success():
 
 @skip_missing_dep
 def test_resp_schema_validation_failure():
-    with pytest.raises(falcon.HTTPInternalServerError) as err:
+    with pytest.raises(falcon.HTTPInternalServerError) as excinfo:
         Resource().response_validated(GoodData(), BadData())
-        assert err.title == 'Response data failed validation'
+
+    assert excinfo.value.title == 'Response data failed validation'
+
+
+@skip_missing_dep
+def test_both_schemas_validation_success():
+    req_data = GoodData()
+    resp_data = GoodData()
+
+    result = Resource().both_validated(req_data, resp_data)
+
+    assert result[0] is req_data
+    assert result[1] is resp_data
+
+    client = testing.TestClient(falcon.API())
+    client.app.add_route('/test', Resource())
+    result = client.simulate_put('/test', json=GoodData.media)
+    assert result.json == resp_data.media
+
+
+@skip_missing_dep
+def test_both_schemas_validation_failure():
+    with pytest.raises(falcon.HTTPInternalServerError) as excinfo:
+        Resource().both_validated(GoodData(), BadData())
+
+    assert excinfo.value.title == 'Response data failed validation'
+
+    with pytest.raises(falcon.HTTPBadRequest) as excinfo:
+        Resource().both_validated(BadData(), GoodData())
+
+    assert excinfo.value.title == 'Request data failed validation'
+
+    client = testing.TestClient(falcon.API())
+    client.app.add_route('/test', Resource())
+    result = client.simulate_put('/test', json=BadData.media)
+    assert result.status_code == 400
