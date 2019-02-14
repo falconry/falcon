3d68e56513638895c839a67a3c29aca330905417
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2019-02-14 05:15:48 +0100

    feat(testing): allow path with a query string in simulate_request (#1433)
    * feat(testing): allow path with a query string in simulate_request
    
    * doc(testing.client): Use a period instead of a comma
    
    * doc(testing.client): Replace "can" with "may" since we are talking about what is allowed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -524,10 +524,21 @@ class TestFalconTestingUtils(object):
 
     def test_query_string_in_path(self):
         app = falcon.API()
-        app.add_route('/', testing.SimpleTestResource())
+        resource = testing.SimpleTestResource()
+        app.add_route('/thing', resource)
         client = testing.TestClient(app)
+
         with pytest.raises(ValueError):
-            client.simulate_get(path='/thing?x=1')
+            client.simulate_get(path='/thing?x=1', query_string='things=1,2,3')
+        with pytest.raises(ValueError):
+            client.simulate_get(path='/thing?x=1', params={'oid': 1978})
+        with pytest.raises(ValueError):
+            client.simulate_get(path='/thing?x=1', query_string='things=1,2,3',
+                                params={'oid': 1978})
+
+        client.simulate_get(path='/thing?detailed=no&oid=1337')
+        assert resource.captured_req.path == '/thing'
+        assert resource.captured_req.query_string == 'detailed=no&oid=1337'
 
     @pytest.mark.parametrize('document', [
         # NOTE(vytas): using an exact binary fraction here to avoid special
