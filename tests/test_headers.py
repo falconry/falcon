75824bab0397a933b8b4c089334062738920917f
Kurt Griffiths <mail@kgriffs.com>
2019-02-14 08:49:39 -0700

    feat: provide a way to set raw cookie headers (#1414)
    Previously, setting raw cookies was not supported without having
    to subclass Response and override several methods. This patch
    adds official support for this functionality, while also explicitly
    disallowing the use of Set-Cookie with certain generic header
    manipulation methods that have semantics that are incompatible
    with Set-Cookie.
    
    BREAKING CHANGE: Previously, several methods in the Response class
    	could be used to attempt to set raw cookie headers. However,
    	due to the Set-Cookie header values not being combinable
    	as a comma-delimited list, this resulted in an unexpected and/or
    	incorrect response being constructed for the user agent in
    	the case that more than one cookie was being set.
    
    Fixes #1265

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -181,6 +181,11 @@ class AppendHeaderResource(object):
     def on_post(self, req, resp):
         resp.append_header('X-Things', 'thing-1')
 
+        c1 = 'ut_existing_user=1; expires=Mon, 14-Jan-2019 21:20:08 GMT; Max-Age=600; path=/'
+        resp.append_header('Set-Cookie', c1)
+        c2 = 'partner_source=deleted; expires=Thu, 01-Jan-1970 00:00:01 GMT; Max-Age=0'
+        resp.append_header('seT-cookie', c2)
+
 
 class RemoveHeaderResource(object):
     def __init__(self, with_double_quotes):
@@ -502,6 +507,27 @@ class TestHeaders(object):
 
         result = client.simulate_request(method='POST')
         assert result.headers['x-things'] == 'thing-1'
+        assert result.cookies['ut_existing_user'].max_age == 600
+        assert result.cookies['partner_source'].max_age == 0
+
+    @pytest.mark.parametrize('header_name', ['Set-Cookie', 'set-cookie', 'seT-cookie'])
+    @pytest.mark.parametrize('error_type', [ValueError, falcon.HeaderNotSupported])
+    def test_set_cookie_disallowed(self, client, header_name, error_type):
+        resp = falcon.Response()
+
+        cookie = 'ut_existing_user=1; expires=Mon, 14-Jan-2019 21:20:08 GMT; Max-Age=600; path=/'
+
+        with pytest.raises(error_type):
+            resp.set_header(header_name, cookie)
+
+        with pytest.raises(error_type):
+            resp.set_headers([(header_name, cookie)])
+
+        with pytest.raises(error_type):
+            resp.get_header(header_name)
+
+        with pytest.raises(error_type):
+            resp.delete_header(header_name)
 
     def test_vary_star(self, client):
         client.app.add_route('/', VaryHeaderResource(['*']))
