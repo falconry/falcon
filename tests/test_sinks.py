d31c661250950294f169cee7423597c81ae08836
cbefus <crbefus@gmail.com>
2019-02-19 18:53:44 -0600

    test: added builder app test doubles for test_sinks
    Increase confidence that the new builder doesn't break the functionality of the old method.

--- tests/test_sinks.py
+++ tests/test_sinks.py
@@ -128,3 +128,115 @@ class TestDefaultRouting(object):
         response = client.simulate_request(path='/books/123')
         assert resource.called
         assert response.status == falcon.HTTP_200
+
+
+class TestDefaultRoutingWithBuilderClient(object):
+
+    def test_single_default_pattern(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_sink(sink) \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/')
+        assert response.status == falcon.HTTP_503
+
+    def test_single_simple_pattern(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_sink(sink, r'/foo') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/foo/bar')
+        assert response.status == falcon.HTTP_503
+
+    def test_single_compiled_pattern(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_sink(sink, re.compile(r'/foo')) \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/foo/bar')
+        assert response.status == falcon.HTTP_503
+
+        response = client.simulate_request(path='/auth')
+        assert response.status == falcon.HTTP_404
+
+    def test_named_groups(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_sink(sink, r'/user/(?P<id>\d+)') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/user/309')
+        assert response.status == falcon.HTTP_503
+        assert sink.kwargs['id'] == '309'
+
+        response = client.simulate_request(path='/user/sally')
+        assert response.status == falcon.HTTP_404
+
+    def test_multiple_patterns(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_sink(sink, r'/foo') \
+            .add_sink(sink_too, r'/foo') \
+            .add_sink(sink, r'/katza') \
+            .build()  # Last duplicate wins
+
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/foo/bar')
+        assert response.status == falcon.HTTP_781
+
+        response = client.simulate_request(path='/katza')
+        assert response.status == falcon.HTTP_503
+
+    def test_with_route(self, sink, resource):
+        app = falcon.APIBuilder() \
+            .add_get_route('/books', resource.on_get) \
+            .add_sink(sink, '/proxy') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/proxy/books')
+        assert not resource.called
+        assert response.status == falcon.HTTP_503
+
+        response = client.simulate_request(path='/books')
+        assert resource.called
+        assert response.status == falcon.HTTP_200
+
+    def test_route_precedence(self, sink, resource):
+        # NOTE(kgriffs): In case of collision, the route takes precedence.
+        app = falcon.APIBuilder() \
+            .add_get_route('/books', resource.on_get) \
+            .add_sink(sink, '/books') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/books')
+        assert resource.called
+        assert response.status == falcon.HTTP_200
+
+    def test_route_precedence_with_id(self, sink, resource):
+        # NOTE(kgriffs): In case of collision, the route takes precedence.
+        app = falcon.APIBuilder() \
+            .add_get_route('/books/{id}', resource.on_get) \
+            .add_sink(sink, '/books') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/books')
+        assert not resource.called
+        assert response.status == falcon.HTTP_503
+
+    def test_route_precedence_with_both_id(self, sink, resource):
+        # NOTE(kgriffs): In case of collision, the route takes precedence.
+        app = falcon.APIBuilder() \
+            .add_get_route('/books/{id}', resource.on_get) \
+            .add_sink(sink, r'/books/\d+') \
+            .build()
+        client = testing.TestClient(app)
+
+        response = client.simulate_request(path='/books/123')
+        assert resource.called
+        assert response.status == falcon.HTTP_200
