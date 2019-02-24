98ddd5f86f1326ddca6c70d08b9b87c106361c1c
Kurt Griffiths <mail@kgriffs.com>
2019-02-23 18:44:55 -0700

    feat(etag): Improve etag performance and docs, plus usability of the ETag class

--- falcon/util/structures.py
+++ falcon/util/structures.py
@@ -108,22 +108,114 @@ class CaseInsensitiveDict(MutableMapping):  # pragma: no cover
 
 
 class ETag(str):
-    """An entity-tag ``str``-like object with the weakness indicator.
+    """Convenience class to represent a parsed HTTP entity-tag.
+
+    This class is simply a subclass of ``str`` with a few helper methods and
+    an extra attribute to indicate whether the entity-tag is weak or strong. The
+    value of the string is equivalent to what RFC 7232 calls an "opaque-tag",
+    i.e. an entity-tag sans quotes and the weakness indicator.
+
+    Note:
+
+        Given that a weak entity-tag comparison can be performed by
+        using the ``==`` operator (per the example below), only a
+        :meth:`~.strong_compare` method is provided.
+
+    Here is an example ``on_get()`` method that demonstrates how to use instances
+    of this class::
+
+        def on_get(self, req, resp):
+            content_etag = self._get_content_etag()
+            for etag in (req.if_none_match or []):
+                if etag == '*' or etag == content_etag:
+                    resp.status = falcon.HTTP_304
+                    return
+
+            # ...
+
+            resp.etag = content_etag
+            resp.status = falcon.HTTP_200
+
+    (See also: RFC 7232)
 
     Attributes:
-        is_weak (bool): The weakness indicator.
+        is_weak (bool): ``True`` if the entity-tag is weak, otherwise ``False``.
 
     """
+
     is_weak = False
 
-    def to_header(self):
-        """Convert the ETag into a HTTP header string.
+    def strong_compare(self, other):
+        """Performs a strong entity-tag comparison.
+
+        Two entity-tags are equivalent if both are not weak and their
+        opaque-tags match character-by-character.
+
+        (See also: RFC 7232, Section 2.3.2)
+
+        Arguments:
+            other (ETag): The other :class:`~.ETag` to which you are comparing
+            this one.
+
+        Returns:
+            bool: ``True`` if the two entity-tags match, otherwise ``False``.
+
+        """
+
+        return self == other and not (self.is_weak or other.is_weak)
+
+    def dumps(self):
+        """Serialize the ETag to a string suitable for use in a precondition header.
+
+        (See also: RFC 7232, Section 2.3)
 
         Returns:
             str: An opaque quoted string, possibly prefixed by a weakness
             indicator ``W/``.
+        """
+
+        if self.is_weak:
+            # PERF(kgriffs): Simple concatenation like this is slightly faster
+            #   than %s string formatting.
+            return 'W/"' + self + '"'
+
+        return '"' + self + '"'
+
+    @classmethod
+    def loads(cls, etag_str):
+        """Class method that deserializes a single entity-tag string from a precondition header.
+
+        Note:
+
+            This method is meant to be used only for parsing a single
+            entity-tag. It can not be used to parse a comma-separated list of
+            values.
+
+        (See also: RFC 7232, Section 2.3)
+
+        Arguments:
+            etag_str (str): An ASCII string representing a single entity-tag,
+                as defined by RFC 7232.
+
+        Returns:
+            ETag: An instance of `~.ETag` representing the parsed entity-tag.
 
         """
-        if self.is_weak:
-            return 'W/"%s"' % self
-        return '"%s"' % self
+
+        value = etag_str
+
+        is_weak = False
+        if value.startswith(('W/', 'w/')):
+            is_weak = True
+            value = value[2:]
+
+        # NOTE(kgriffs): We allow for an unquoted entity-tag just in case,
+        #   although it has been non-standard to do so since at least 1999
+        #   with the advent of RFC 2616.
+        if value[:1] == value[-1:] == '"':
+            value = value[1:-1]
+
+        t = cls(value)
+        t.is_weak = is_weak
+
+        return t
