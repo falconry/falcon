08c541f7bc7e245be1e4780fc9172f0d9cabcf6e
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2019-03-06 19:17:29 +0100

    docs(setup): improve data and code separation in patron list substitution

--- setup.py
+++ setup.py
@@ -74,7 +74,8 @@ else:
 
 
 def load_description():
-    in_patrons = False
+    in_patron_list = False
+    in_patron_replacement = False
     in_raw = False
 
     description_lines = []
@@ -83,16 +84,17 @@ def load_description():
     for readme_line in io.open('README.rst', 'r', encoding='utf-8'):
 
         # NOTE(vytas): The patron list largely builds upon raw sections
-        if 'big thank you to our patrons' in readme_line.strip().lower():
-            in_patrons = True
-            description_lines.append('Support Falcon Development\n')
-            description_lines.append('--------------------------\n')
+        if readme_line.startswith('.. Patron list starts'):
+            in_patron_list = True
+            in_patron_replacement = True
             continue
-        elif in_patrons and readme_line.startswith('---'):
-            in_patrons = False
-            continue
-        elif readme_line.rstrip().lower() in (
-                'platinum', 'gold', 'silver', 'bronze', 'nickel', 'iron'):
+        elif in_patron_list:
+            if not readme_line.strip():
+                in_patron_replacement = False
+            elif in_patron_replacement:
+                description_lines.append(readme_line.lstrip())
+            if readme_line.startswith('.. Patron list ends'):
+                in_patron_list = False
             continue
         elif readme_line.startswith('.. raw::'):
             in_raw = True
