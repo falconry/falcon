4fced4ababff1b4c1bab690ed82b62b9e548441e
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2019-05-06 18:41:33 -0400

    Exclude the _newsfragments directory

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ release = falcon.__version__ + tag
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '_newsfragments']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
