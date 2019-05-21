1bb410441609dbf2d7b6fb0f8fa24769bfab7db8
coykto <coykto@gmail.com>
2019-05-21 11:52:03 +0500

    fixed tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -82,12 +82,14 @@ class RequireJSON:
     def process_request(self, req, resp):
         if not req.client_accepts_json:
             raise falcon.HTTPNotAcceptable(
+                title='406 Not Acceptable',
                 description='This API only supports responses encoded as JSON.',
                 href='http://docs.examples.com/api/json')
 
         if req.method in ('POST', 'PUT'):
             if 'application/json' not in req.content_type:
                 raise falcon.HTTPUnsupportedMediaType(
+                    title='415 Unsupported Media Type',
                     description='This API only supports requests encoded as JSON.',
                     href='http://docs.examples.com/api/json')
 
