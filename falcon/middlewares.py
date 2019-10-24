4e676ab24553388f993fb3265b37eff362efb979
Kurt Griffiths <mail@kgriffs.com>
2019-10-23 21:35:49 -0600

    doc(middlewares): Edit docstring for clarity

--- falcon/middlewares.py
+++ falcon/middlewares.py
@@ -1,34 +1,28 @@
 
 class CORSMiddleware(object):
     def process_response(self, req, resp, resource, req_succeeded):
-        """
-        CORS Middleware out of the box in falcon to turn on/off the CORS
-        Headers on preflight validation from the browser.
+        """Implement a simple blanked CORS policy for all routes.
 
-        Args:
-            req: Current request object to check method was used.
-            resp: Current response object to add headers for CORS if
-                they does not exists
-            resource:
-            req_succeeded: Flag to check if the response was handled
-                successfully or there have been an error
+        This middleware provides a simple out-of-the box CORS policy,
+        including handling of preflighted requests from the browser.
+
+        See also: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
         """
+
         if resp.get_header('Access-Control-Allow-Origin') is None:
             resp.set_header('Access-Control-Allow-Origin', '*')
 
         if (req_succeeded and
                 req.method == 'OPTIONS' and
                 req.get_header('Access-Control-Request-Method')):
+
             # NOTE(kgriffs): This is a CORS preflight request. Patch the
             #   response accordingly.
 
             allow = resp.get_header('Allow')
             resp.delete_header('Allow')
 
-            allow_headers = req.get_header(
-                'Access-Control-Request-Headers',
-                default='*'
-            )
+            allow_headers = req.get_header('Access-Control-Request-Headers', default='*')
 
             resp.set_header('Access-Control-Allow-Methods', allow)
             resp.set_header('Access-Control-Allow-Headers', allow_headers)
