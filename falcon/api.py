63d2b09e2447110f47f9b3d461dc708c0f87727d
Kurt Griffiths <mail@kgriffs.com>
2019-10-23 20:16:02 -0600

    doc(api): Fix typo in cors_enable docstring

--- falcon/api.py
+++ falcon/api.py
@@ -140,7 +140,7 @@ class API:
         cors_enable (bool): Set this flag to ``True`` to enable a simple
             CORS policy for all routes, including support for`preflight` 
             requests (default ``False``). 
-            (See also: :ref:`CORSs <cors>`)
+            (See also: :ref:`CORS <cors>`)
 
     Attributes:
         req_options: A set of behavioral options related to incoming
