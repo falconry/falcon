ee1b91afbfe8f2102d30e1aaffc49ac6e041eac9
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2019-12-16 19:07:22 +0100

    chore(coverage): pin coverage < 5.0 until coveralls supports it

--- tools/mintest.sh
+++ tools/mintest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-pip install -U tox coverage
+# TODO(vytas): unpin coverage to allow 5.x+ when coveralls supports it
+# https://github.com/coveralls-clients/coveralls-python/issues/203
+pip install -U tox "coverage < 5.0"
 
 rm -f .coverage.*
 tox -e pep8 && tox -e py38 && tools/testing/combine_coverage.sh
