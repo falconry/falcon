460e1ceebe7f6e41da914d3cfffed711df6c4221
Federico Caselli <cfederico87@gmail.com>
2020-02-04 23:21:09 +0100

    test: add test for mimetypes inited

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,6 +1,7 @@
 import pytest
+from unittest.mock import MagicMock
 
-from falcon import MEDIA_TEXT
+from falcon import MEDIA_TEXT, ResponseOptions
 
 from _util import create_resp  # NOQA
 
@@ -58,3 +59,18 @@ def test_response_removed_stream_len(resp):
 
     with pytest.raises(AttributeError):
         resp.stream_len
+
+
+def test_response_option_mimetype_init(monkeypatch):
+    mock = MagicMock()
+    mock.inited = False
+    monkeypatch.setattr('falcon.response.mimetypes', mock)
+
+    ro = ResponseOptions()
+
+    assert ro.static_media_types is mock.types_map
+    mock.reset_mock()
+    mock.inited = True
+    ro = ResponseOptions()
+    assert ro.static_media_types is mock.types_map
+    mock.init.assert_not_called()
