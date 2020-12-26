4dd07e117eed4f44a271230490cf93aab7bb8aa8
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2020-12-26 18:18:10 +0100

    perf(CI): source a new version of cachegrind-benchmarking

--- perf/cachegrind.py
+++ perf/cachegrind.py
@@ -1,6 +1,6 @@
 """
 As per the original author's recommendation, this script was simply copied from
-https://github.com/pythonspeed/cachegrind-benchmarking @ b9dabb6c.
+https://github.com/pythonspeed/cachegrind-benchmarking @ 32d26691.
 
 See also this awesome article by Itamar Turner-Trauring:
 https://pythonspeed.com/articles/consistent-benchmarking-in-ci/.
@@ -9,13 +9,12 @@ The original file content follows below.
 
 -------------------------------------------------------------------------------
 
-Proof-of-concept: run a program under Cachegrind, combining all the various
+Proof-of-concept: run_with_cachegrind a program under Cachegrind, combining all the various
 metrics into one single performance metric.
 
 Requires Python 3.
 
 License: https://opensource.org/licenses/MIT
-
 
 ## Features
 
@@ -30,32 +29,37 @@ https://pythonspeed.com/articles/consistent-benchmarking-in-ci/
 ## Usage
 
 This script has no compatibility guarnatees, I recommend copying it into your
-repository. To use:
+repository.  To use:
 
 $ python3 cachegrind.py ./yourprogram --yourparam=yourvalues
 
-The last line printed will be a combined performance metric.
+If you're benchmarking Python, make sure to set PYTHONHASHSEED to a fixed value
+(e.g. `export PYTHONHASHSEED=1234`).  Other languages may have similar
+requirements to reduce variability.
 
+The last line printed will be a combined performance metric, but you can tweak
+the script to extract more info, or use it as a library.
 
 Copyright © 2020, Hyphenated Enterprises LLC.
 """
 
 from typing import List, Dict
-from subprocess import check_output, PIPE, Popen
-import re
+from subprocess import check_call, check_output
 import sys
+from tempfile import NamedTemporaryFile
 
 ARCH = check_output(["uname", "-m"]).strip()
 
 
-def _run(args_list: List[str]) -> Dict[str, int]:
+def run_with_cachegrind(args_list: List[str]) -> Dict[str, int]:
     """
     Run the the given program and arguments under Cachegrind, parse the
     Cachegrind specs.
 
     For now we just ignore program output, and in general this is not robust.
     """
-    complete_args = [
+    temp_file = NamedTemporaryFile("r+")
+    check_call([
         # Disable ASLR:
         "setarch",
         ARCH,
@@ -68,66 +72,65 @@ def _run(args_list: List[str]) -> Dict[str, int]:
         "--I1=32768,8,64",
         "--D1=32768,8,64",
         "--LL=8388608,16,64",
-    ] + args_list
-    popen = Popen(complete_args, stderr=PIPE, universal_newlines=True)
-    stderr = popen.stderr.read()
-    popen.wait()
+        "--cachegrind-out-file=" + temp_file.name,
+    ] + args_list)
+    return parse_cachegrind_output(temp_file)
 
-    # Discovered afterwards we can parse the cachegrind.out.<pid> file's last
-    # line. Oh well, maybe in rewrite.
-    result = {}
-    for line in stderr.splitlines():
-        if re.match("^==[0-9]*== ", line):
-            match = re.match("^==[0-9]*== ([ILD][A-Za-z0-9 ]*):  *([0-9,]*)", line)
-            if match:
-                name, value = match.groups()
-                # Drop extra spaces:
-                name = " ".join(name.split())
-                # Convert "123,456" into integer:
-                value = int(value.replace(",", ""))
-                result[name] = value
-        sys.stderr.write(line + "\n")
-    return result
+
+def parse_cachegrind_output(temp_file):
+    # Parse the output file:
+    lines = iter(temp_file)
+    for line in lines:
+        if line.startswith("events: "):
+            header = line[len("events: "):].strip()
+            break
+    for line in lines:
+        last_line = line
+    assert last_line.startswith("summary: ")
+    last_line = last_line[len("summary:"):].strip()
+    return dict(zip(header.split(), [int(i) for i in last_line.split()]))
 
 
 def get_counts(cg_results: Dict[str, int]) -> Dict[str, int]:
     """
-    Given the result of _run(), figure out the parameters we will use for final
+    Given the result of run_with_cachegrind(), figure out the parameters we will use for final
     estimate.
 
     We pretend there's no L2 since Cachegrind doesn't currently support it.
 
     Caveats: we're not including time to process instructions, only time to
-    access instruction cache(s), so we're assuming time to fetch and run
+    access instruction cache(s), so we're assuming time to fetch and run_with_cachegrind
     instruction is the same as time to retrieve data if they're both to L1
     cache.
     """
     result = {}
+    d = cg_results
 
-    ram_hits = cg_results["LL misses"]
-    assert ram_hits == cg_results["LLi misses"] + cg_results["LLd misses"]
+    ram_hits = d["DLmr"] + d["DLmw"] + d["ILmr"]
 
-    l3_hits = cg_results["LL refs"]
-    assert l3_hits == cg_results["I1 misses"] + cg_results["D1 misses"]
+    l3_hits = d["I1mr"] + d["D1mw"] + d["D1mr"] - ram_hits
 
-    total_memory_rw = cg_results["I refs"] + cg_results["D refs"]
+    total_memory_rw = d["Ir"] + d["Dr"] + d["Dw"]
     l1_hits = total_memory_rw - l3_hits - ram_hits
+    assert total_memory_rw == l1_hits + l3_hits + ram_hits
 
     result["l1"] = l1_hits
     result["l3"] = l3_hits
     result["ram"] = ram_hits
+
     return result
 
 
 def combined_instruction_estimate(counts: Dict[str, int]) -> int:
     """
-    Given the result of _run(), return estimate of total time to run.
+    Given the result of run_with_cachegrind(), return estimate of total time to run_with_cachegrind.
 
     Multipliers were determined empirically, but some research suggests they're
-    a reasonable approximation for cache time ratios.
+    a reasonable approximation for cache time ratios.  L3 is probably too low,
+    but then we're not simulating L2...
     """
-    return counts["l1"] + (5 * counts["l3"]) + (30 * counts["ram"])
+    return counts["l1"] + (5 * counts["l3"]) + (35 * counts["ram"])
 
 
 if __name__ == "__main__":
-    print(combined_instruction_estimate(get_counts(_run(sys.argv[1:]))))
+    print(combined_instruction_estimate(get_counts(run_with_cachegrind(sys.argv[1:]))))
