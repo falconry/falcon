485d6fa24c5e7be2b579b64e736259e1f628c967
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2021-02-23 16:42:08 +0100

    test(E2E): add basic E2E tests for SSE & WebSocket (#1869)
    * test(E2E): sketch an E2E skeleton
    
    * test(E2E): improve static files and their serving (WiP)
    
    * test(e2e): commit parts of a more functional WiP prototype
    
    * test(E2E): add WebSockets
    
    * test(e2e): actually write some semi-useful tests
    
    * test(e2e): try running E2E in Actions
    
    * style(imports): address import order violations
    
    * test(E2E): address a part of review comments

---
+++ e2e-tests/server/__init__.py
@@ -0,0 +1,5 @@
+from .app import create_app
+
+__all__ = ['app']
+
+app = create_app()
---
+++ e2e-tests/test_e2e.py
@@ -0,0 +1,74 @@
+# Copyright 2020-2021 by Vytautas Liuolia.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_sse_broadcast(browser, clear_log):
+    browser.slow_click('#button1')
+    browser.assert_text('WS1 CONNECTED', 'div.sse', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/all Zombie alert!')
+    browser.slow_click('#button1')
+    browser.assert_text('[WS1] Zombie alert!', 'div.sse', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/all Zombie apocalypse averted (for now)')
+    browser.slow_click('#button1')
+    browser.assert_text(
+        '[WS1] Zombie apocalypse averted (for now)', 'div.sse', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/quit')
+    browser.slow_click('#button1')
+    browser.assert_text('Bye, WS1!', 'div.ws1', timeout=5)
+    browser.assert_text('WS1 DISCONNECTED', 'div.sse', timeout=5)
+
+
+def test_chat(browser, clear_log):
+    browser.slow_click('#button1')
+    browser.assert_text('Hello, WS1!', 'div.ws1', timeout=5)
+    browser.assert_text('WS1 CONNECTED', 'div.sse', timeout=5)
+    clear_log()
+
+    browser.slow_click('#button2')
+    browser.assert_text('Hello, WS2!', 'div.ws2', timeout=5)
+    browser.assert_text('WS2 CONNECTED', 'div.sse', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/msg WS2 Apples')
+    browser.slow_click('#button1')
+    browser.assert_text('[WS1] Apples', 'div.ws2', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/msg WS2 Oranges')
+    browser.slow_click('#button1')
+    browser.assert_text('[WS1] Oranges', 'div.ws2', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/msg WS2 Bananas')
+    browser.slow_click('#button1')
+    browser.assert_text('[WS1] Bananas', 'div.ws2', timeout=5)
+    clear_log()
+
+    browser.type('#input2', '/msg WS1 Talk to you later...')
+    browser.slow_click('#button2')
+    browser.assert_text('[WS2] Talk to you later...', 'div.ws1', timeout=5)
+    clear_log()
+
+    browser.type('#input1', '/quit')
+    browser.slow_click('#button1')
+    browser.assert_text('Bye, WS1!', 'div.ws1', timeout=5)
+    browser.type('#input2', '/quit')
+    browser.slow_click('#button2')
+    browser.assert_text('Bye, WS2!', 'div.ws2', timeout=5)
