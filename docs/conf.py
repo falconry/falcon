c93e443b2a92835d9719fe58a9066f4467311ba4
Federico Caselli <cfederico87@gmail.com>
2021-05-29 00:44:31 +0200

    style: re-run black

--- docs/conf.py
+++ docs/conf.py
@@ -329,9 +329,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'falcon', 'Falcon Documentation', ['Kurt Griffiths et al.'], 1)
-]
+man_pages = [('index', 'falcon', 'Falcon Documentation', ['Kurt Griffiths et al.'], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
