fd7fb73e47b757e2a1633381abb8465ec9dcfebc
Federico Caselli <cfederico87@gmail.com>
2021-08-06 10:35:59 +0200

    chore: re-enable api_helpers deprecations

--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -2,7 +2,7 @@ import pytest
 
 from falcon import app_helpers, request_helpers, stream
 
-# from _util import has_cython
+from _util import has_cython
 
 
 def test_bounded_stream():
@@ -18,17 +18,16 @@ class TestApiHelpers:
         for name in app_helpers.__all__:
             assert getattr(api_helpers, name) is getattr(app_helpers, name)
 
-    # TODO enable test of deprecation
-    # @pytest.mark.skipif(
-    #     has_cython,
-    #     reason='Reloading modules on Cython does not work',
-    # )
-    # def test_warning(self):
-    #     import importlib
+    @pytest.mark.skipif(
+        has_cython,
+        reason='Reloading modules on Cython does not work',
+    )
+    def test_warning(self):
+        import importlib
 
-    #     from falcon.util.deprecation import DeprecatedWarning
+        from falcon.util.deprecation import DeprecatedWarning
 
-    #     with pytest.warns(DeprecatedWarning, match='The api_helpers'):
-    #         from falcon import api_helpers
+        with pytest.warns(DeprecatedWarning, match='The api_helpers'):
+            from falcon import api_helpers
 
-    #         importlib.reload(api_helpers)
+            importlib.reload(api_helpers)
