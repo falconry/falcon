6ccc0a61fea1e7b2bcfe1cbb99994dc9d08eaf34
Jonathan DiMuccio <a1425@a1425s-MacBook-Pro.local>
2021-08-06 17:35:25 -0400

    add finally

--- falcon/asgi/app.py
+++ falcon/asgi/app.py
@@ -698,9 +698,9 @@ class App(falcon.app.App):
                         '__aiter__ method. Error raised while iterating over '
                         'Response.stream: ' + str(ex)
                     )
-
-            if hasattr(stream, 'close'):
-                await stream.close()
+                finally:
+                    if hasattr(stream, 'close'):
+                        await stream.close()
 
         await send(_EVT_RESP_EOF)
 
