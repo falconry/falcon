39e4822ddc1f063d81ff8dd84d2837c132330f72
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2022-02-06 11:20:27 +0100

    chore(tools): append contributors to AUTHORS

--- tools/add_contributors.py
+++ tools/add_contributors.py
@@ -15,6 +15,9 @@ FALCON_REPOSITORY_API = 'https://api.github.com/repos/falconry/falcon'
 
 STABLE_RELEASE_TAG = r'^\d+\.\d+\.\d+(\.post\d+)?$'
 
+AUTHORS_SEPARATOR = '\n(et al.)\n\n'
+AUTHORS_LINE = r'^\* (?:(?:.+ \(([\w-]+)\)$)|([\w-]+$))'
+
 
 def get_latest_tag():
     uri = f'{FALCON_REPOSITORY_API}/tags'
@@ -38,31 +41,53 @@ def iter_commits(until=None):
 
 
 def aggregate_contributors(until=None):
-    authors = []
+    result = {}
     for commit in iter_commits(until):
         login = commit['author'].get('login')
         if not login:
             continue
-        authors.append((login, commit['commit']['author']['name']))
+        if login in result:
+            result.pop(login)
+        # NOTE(vytas): Exploit dictionary ordering in Python 3.7+.
+        result[login] = commit['commit']['author']['name']
 
-    seen = set()
-    result = {}
-    for login, name in reversed(authors):
-        if login not in seen:
-            seen.add(login)
-        result[login] = name
-
-    return result
+    return dict(item for item in reversed(result.items()))
 
 
 def _get_towncrier_filename():
-    with open(ROOT / 'pyproject.toml') as pyproject_toml:
+    with open(ROOT / 'pyproject.toml', 'r') as pyproject_toml:
         project = toml.load(pyproject_toml)
     return project['tool']['towncrier']['filename']
 
 
 def _update_authors(contributors):
-    pass
+    with open(ROOT / 'AUTHORS', 'r') as authors_file:
+        content = authors_file.read()
+
+    authors, separator, footer = content.partition(AUTHORS_SEPARATOR)
+    assert separator, 'AUTHORS file structure not understood, please inspect manually'
+
+    existing = set({FALCON_CREATOR})
+    for line in reversed(authors.splitlines()):
+        match = re.match(AUTHORS_LINE, line)
+        if not match:
+            break
+        login = match.group(1) or match.group(2)
+        existing.add(login.lower())
+
+    with open(ROOT / 'AUTHORS', 'w') as authors_file:
+        authors_file.write(authors)
+
+        for login, name in contributors.items():
+            if login.lower() in existing:
+                continue
+            if login == name:
+                authors_file.write(f'* {login}\n')
+            else:
+                authors_file.write(f'* {name} ({login})\n')
+
+        authors_file.write(separator)
+        authors_file.write(footer)
 
 
 def _update_towncrier_template(template, contributors):
