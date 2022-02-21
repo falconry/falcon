d180f994109f92c7311070415b3b031149f4dff8
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2022-02-21 20:46:41 +0100

    chore(mailman): use the latest mailman `master`

--- tools/testing/fetch_mailman.sh
+++ tools/testing/fetch_mailman.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 MAILMAN_PATH=.ecosystem/mailman
-# TODO(vytas): Detect from git tags or PyPi JSON.
+
+# TODO(vytas): Detect the latest version from git tags or PyPi JSON.
 # NOTE(vytas): One approach: `curl -Ls https://pypi.org/pypi/mailman/json | jq -r .info.version`
-MAILMAN_VERSION=3.3.5
+# MAILMAN_VERSION=3.3.5
 
 # Clean up in case we are running locally and not in CI
 rm -rf $MAILMAN_PATH
 
 mkdir -p .ecosystem
-git clone https://gitlab.com/mailman/mailman $MAILMAN_PATH
+git clone https://gitlab.com/mailman/mailman.git/ $MAILMAN_PATH
 
-cd $MAILMAN_PATH
-git checkout tags/$MAILMAN_VERSION
+# TODO(vytas): Enable version checking when a stable release's tests pass as-is.
+#   At the time of writing, the latest version tag (3.3.5) has some failing tests.
+# cd $MAILMAN_PATH
+# git checkout tags/$MAILMAN_VERSION
