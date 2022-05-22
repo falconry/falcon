bc6f4a808d674e55cee8b1169ef7d97ed0771c20
Nurbek Po'latov <polatovnurbek622@gmail.com>
2022-05-22 20:03:47 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from aiogram import executor
+
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
+
+
+async def on_startup(dispatcher):
+    # Birlamchi komandalar (/star va /help)
+    await set_default_commands(dispatcher)
+
+    # Bot ishga tushgani haqida adminga xabar berish
+    await on_startup_notify(dispatcher)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
