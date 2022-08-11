c76f44248af3f44d92b9658bcf7abecd3e1fb038
Kurt Griffiths <mail@kgriffs.com>
2022-08-11 04:54:18 -0600

    chore: remove deprecated api_helpers module and the deprecated body attrs (#2090)
    * remove deprecated
    
    * Delete api_helpers.py
    
    * Update test_deprecations.py
    
    * chore: raise error when attempting to access body attributes
    
    * refactor: change RemovedError to inherit from AttributeError
    
    Co-authored-by: jan zens <89642092+sneznaj@users.noreply.github.com>
    Co-authored-by: Vytautas Liuolia <vytautas.liuolia@gmail.com>

--- falcon/api_helpers.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-from .app_helpers import *  # NOQA
-from .util.deprecation import DeprecatedWarning
-
-warnings.warn('The api_helpers module was renamed to app_helpers.', DeprecatedWarning)
--- tests/test_deprecations.py
+++ tests/test_deprecations.py
@@ -1,33 +1,6 @@
-import pytest
-
-from falcon import app_helpers, request_helpers, stream
-
-from _util import has_cython  # NOQA
+from falcon import request_helpers, stream
 
 
 def test_bounded_stream():
     assert request_helpers.Body is stream.Body
     assert request_helpers.BoundedStream is stream.BoundedStream
-
-
-class TestApiHelpers:
-    @pytest.mark.filterwarnings('ignore:The api_helpers')
-    def test_imports(self):
-        from falcon import api_helpers
-
-        for name in app_helpers.__all__:
-            assert getattr(api_helpers, name) is getattr(app_helpers, name)
-
-    @pytest.mark.skipif(
-        has_cython,
-        reason='Reloading modules on Cython does not work',
-    )
-    def test_warning(self):
-        import importlib
-
-        from falcon.util.deprecation import DeprecatedWarning
-
-        with pytest.warns(DeprecatedWarning, match='The api_helpers'):
-            from falcon import api_helpers
-
-            importlib.reload(api_helpers)
