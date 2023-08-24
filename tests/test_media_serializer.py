75baaf5f9bef18901fcb3c6a04ce5f2b5c4f1695
Piotr Kopalko <copalco@gmail.com>
2023-08-24 19:20:18 +0200

    Fixed formatting

--- tests/test_media_serializer.py
+++ tests/test_media_serializer.py
@@ -4,7 +4,6 @@ from falcon.media_serializer import Serializer
 
 
 class TestSerializer:
-
     def test_interface_raises_not_implemented(self) -> None:
         with pytest.raises(NotImplementedError):
             Serializer().serialize({'data': 'any'}, 'any')
