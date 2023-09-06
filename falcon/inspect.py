8b06f45a5aff3bacae1346b1be4b79c0696bdbd7
Piotr Kopalko <copalco@gmail.com>
2023-09-06 20:40:46 +0200

    Revert "Replace Union with | where future annotations imported"
    This reverts commit fd8b3be07ff9471bc774cf17f096092708ec35e6.

--- falcon/inspect.py
+++ falcon/inspect.py
@@ -26,6 +26,7 @@ from typing import List
 from typing import Optional
 from typing import Tuple
 from typing import Type  # NOQA: F401
+from typing import Union
 
 from falcon import app_helpers
 from falcon.app import App
@@ -614,7 +615,7 @@ class StringVisitor(InspectVisitor):
         return text
 
     def _methods_to_string(
-        self, methods: List[RouteMethodInfo] | List[MiddlewareMethodInfo]
+        self, methods: Union[List[RouteMethodInfo], List[MiddlewareMethodInfo]]
     ) -> str:
         """Return a string from the list of methods."""
         tab = self.tab + ' ' * 3
@@ -813,11 +814,13 @@ def _is_internal(obj: Any) -> bool:
 
 
 def _filter_internal(
-    iterable: Iterable[RouteMethodInfo]
-    | Iterable[ErrorHandlerInfo]
-    | Iterable[MiddlewareMethodInfo],
+    iterable: Union[
+        Iterable[RouteMethodInfo],
+        Iterable[ErrorHandlerInfo],
+        Iterable[MiddlewareMethodInfo],
+    ],
     return_internal: bool,
-) -> Iterable[_Traversable] | List[_Traversable]:
+) -> Union[Iterable[_Traversable], List[_Traversable]]:
     """Filter the internal elements of an iterable."""
     if return_internal:
         return iterable
