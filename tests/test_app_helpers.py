f77518ff43a92d5bebf45c6e838999de5d7a2378
Piotr Kopalko <copalco@gmail.com>
2023-11-21 20:33:15 +0100

    Extract options to variables for improved readability

--- tests/test_app_helpers.py
+++ tests/test_app_helpers.py
@@ -6,29 +6,35 @@ from falcon.request import Request
 from falcon.response import Response
 from falcon.testing import create_environ
 
+JSON = ('application/json', 'application/json', b'{"title": "404 Not Found"}')
+XML = (
+    'application/xml',
+    'application/xml',
+    (
+        b'<?xml version="1.0" encoding="UTF-8"?>'
+        b'<error><title>404 Not Found</title></error>'
+    ),
+)
+CUSTOM_JSON = ('custom/any+json', 'application/json', b'{"title": "404 Not Found"}')
+
+CUSTOM_XML = (
+    'custom/any+xml',
+    'application/xml',
+    (
+        b'<?xml version="1.0" encoding="UTF-8"?>'
+        b'<error><title>404 Not Found</title></error>'
+    ),
+)
+
 
 class TestDefaultSerializeError:
     @pytest.mark.parametrize(
         'accept, content_type, data',
         (
-            ('application/json', 'application/json', b'{"title": "404 Not Found"}'),
-            (
-                'application/xml',
-                'application/xml',
-                (
-                    b'<?xml version="1.0" encoding="UTF-8"?>'
-                    b'<error><title>404 Not Found</title></error>'
-                ),
-            ),
-            ('custom/any+json', 'application/json', b'{"title": "404 Not Found"}'),
-            (
-                'custom/any+xml',
-                'application/xml',
-                (
-                    b'<?xml version="1.0" encoding="UTF-8"?>'
-                    b'<error><title>404 Not Found</title></error>'
-                ),
-            ),
+            JSON,
+            XML,
+            CUSTOM_JSON,
+            CUSTOM_XML,
         ),
     )
     def test_serializes_error_to_preffered_by_sender(
