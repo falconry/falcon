dd9210ae060cff13305d4fde59f641f57339fd05
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2024-03-20 17:07:00 +0100

    chore(sync): use `asyncio.Runner` for `async_to_sync()` on py311+

--- falcon/util/sync.py
+++ falcon/util/sync.py
@@ -22,7 +22,27 @@ __all__ = [
     'wrap_sync_to_async_unsafe',
 ]
 
+Result = TypeVar('Result')
+
+
+class _DummyRunner:
+    def run(self, coro: Awaitable[Result]) -> Result:  # pragma: nocover
+        # NOTE(vytas): Work around get_event_loop deprecation in 3.10 by going
+        #   via get_event_loop_policy(). This should be equivalent for
+        #   async_to_sync's use case as it is currently impossible to invoke
+        #   run_until_complete() from a running loop anyway.
+        return self.get_loop().run_until_complete(coro)
+
+    def get_loop(self) -> asyncio.AbstractEventLoop:  # pragma: nocover
+        return asyncio.get_event_loop_policy().get_event_loop()
+
+    def close(self) -> None:  # pragma: nocover
+        pass
+
+
 _one_thread_to_rule_them_all = ThreadPoolExecutor(max_workers=1)
+_runner_cls = getattr(asyncio, 'Runner', _DummyRunner)
+_runner = _runner_cls()
 
 create_task = asyncio.create_task
 get_running_loop = asyncio.get_running_loop
@@ -190,9 +210,6 @@ def _wrap_non_coroutine_unsafe(
     return wrap_sync_to_async_unsafe(func)
 
 
-Result = TypeVar('Result')
-
-
 def async_to_sync(
     coroutine: Callable[..., Awaitable[Result]], *args: Any, **kwargs: Any
 ) -> Result:
@@ -204,8 +221,13 @@ def async_to_sync(
     one will be created.
 
     Warning:
-        This method is very inefficient and is intended primarily for testing
-        and prototyping.
+        Executing async code in this manner is inefficient since it involves
+        synchronization via threading primitives, and is intended primarily for
+        testing, prototyping or compatibility purposes.
+
+    Note:
+        On Python 3.11+, this function leverages a module-wide
+        ``asyncio.Runner``.
 
     Args:
         coroutine: A coroutine function to invoke.
@@ -214,17 +236,12 @@ def async_to_sync(
     Keyword Args:
         **kwargs: Additional args are passed through to the coroutine function.
     """
-
-    # TODO(vytas): The canonical way of doing this for simple use cases is
-    #   asyncio.run(), but that would be a breaking change wrt the above
-    #   documented behaviour; breaking enough to break some of our own tests.
-
-    # NOTE(vytas): Work around get_event_loop deprecation in 3.10 by going via
-    #   get_event_loop_policy(). This should be equivalent for async_to_sync's
-    #   use case as it is currently impossible to invoke run_until_complete()
-    #   from a running loop anyway.
-    loop = asyncio.get_event_loop_policy().get_event_loop()
-    return loop.run_until_complete(coroutine(*args, **kwargs))
+    # NOTE(vytas): Sometimes our runner's loop can get picked and consumed by
+    #   other utilities and test methods. If that happens, recreate the runner.
+    global _runner
+    if _runner.get_loop().is_closed():
+        _runner = _runner_cls()
+    return _runner.run(coroutine(*args, **kwargs))
 
 
 def runs_sync(coroutine: Callable[..., Awaitable[Result]]) -> Callable[..., Result]:
