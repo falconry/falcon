1c25f3d880ddfd1407c9fae4416c0a61dcaa9966
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2024-07-24 10:21:25 +0200

    chore(wslook): shorten comment lines in `client.py`

--- examples/wslook/wslook/client.py
+++ examples/wslook/wslook/client.py
@@ -1,6 +1,6 @@
-# This is a simple example of a WebSocket client that sends a message to the server
-# Since it's an example using the `websockets` library and it isn't using anything specific to Falcon,
-# there are no tests. Coverage is skipped for this module.
+# This is a simple example of a WebSocket client that sends a message to the server.
+# Since it's an example using the `websockets` library, and it isn't using anything specific
+# to Falcon, there are no tests. Coverage is skipped for this module.
 
 import asyncio
 
