bd877fbb47f1be3af6882d82b700fe9f9fb9c48e
Piotr Kopalko <copalco@gmail.com>
2024-08-19 20:38:28 +0200

    Make all status variables final

--- tests/test_status_codes.py
+++ tests/test_status_codes.py
@@ -11,16 +11,16 @@ class TestStatusCodes:
     @pytest.mark.skipif(
         sys.version_info < (3, 13), reason='Outdated http statuses definitions'
     )
-    def test_statuses_are_in_compliance_with_http_from_python313(self):
-        for status in status_codes.__all__:
-            status_code, message = self._status_code_and_message(status)
-            if status_code >= 700:
-                pytest.skip('Codes above 700 are not defined in http package')
-            http_status = http.HTTPStatus(status_code)
-            if status_code in [413, 418, 422]:
-                assert http_status.phrase != message
-            else:
-                assert http_status.phrase == message
+    @pytest.mark.parametrize('status', status_codes.__all__)
+    def test_statuses_are_in_compliance_with_http_from_python313(self, status):
+        status_code, message = self._status_code_and_message(status)
+        if status_code >= 700:
+            pytest.skip('Codes above 700 are not defined in http package')
+        http_status = http.HTTPStatus(status_code)
+        if status_code in [413, 418, 422]:
+            assert http_status.phrase != message
+        else:
+            assert http_status.phrase == message
 
     def _status_code_and_message(self, status: str) -> Tuple[int, str]:
         status = getattr(status_codes, status)
