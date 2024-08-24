1bd1dec8c6ebca76402c1d102b254a795e4b5fea
Piotr Kopalko <copalco@gmail.com>
2024-08-24 12:41:50 +0200

    Simplify has_param method

--- falcon/request.py
+++ falcon/request.py
@@ -1860,10 +1860,7 @@ class Request:
 
         """
 
-        if name in self._params:
-            return True
-        else:
-            return False
+        return name in self._params
 
     def log_error(self, message):
         """Write an error message to the server's log.
