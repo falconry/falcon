b08314d9aceec75b4e4fa3bfef6b2c50c2723757
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2024-10-04 23:45:43 +0200

    docs: remove an empty line from quote.py

--- examples/quote.py
+++ examples/quote.py
@@ -4,7 +4,6 @@ import falcon
 class QuoteResource:
     def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
         """Handle GET requests."""
-
         resp.media = {
             'quote': "I've always been more interested in the future than in the past.",
             'author': 'Grace Hopper',
