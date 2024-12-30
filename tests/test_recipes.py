762194268d38a3f16234324f1d086c226ef37500
EricGoulart <106963488+EricGoulart@users.noreply.github.com>
2024-12-30 06:00:50 -0300

    docs(recipes): add text/plain media handler recipe (#2419)
    * feat(recipes): add text/plain media handler example
    
    * adding the rst file
    
    * fix plain/text handler
    
    * reformatted
    
    * docs(recipes): update plain-text-handler.rst
    
    ---------
    
    Co-authored-by: diegomirandap <diegomirandapinto2003@gmail.com>
    Co-authored-by: diegomirandap <108027980+diegomirandap@users.noreply.github.com>
    Co-authored-by: Vytautas Liuolia <vytautas.liuolia@gmail.com>

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -137,6 +137,31 @@ class TestRawURLPath:
         assert scope2['raw_path'] == url2.encode()
 
 
+class TestTextPlainHandler:
+    class MediaEcho:
+        def on_post(self, req, resp):
+            resp.content_type = req.content_type
+            resp.media = req.get_media()
+
+    def test_text_plain_basic(self, util):
+        recipe = util.load_module('examples/recipes/plain_text_main.py')
+
+        app = falcon.App()
+        app.req_options.media_handlers['text/plain'] = recipe.TextHandler()
+        app.resp_options.media_handlers['text/plain'] = recipe.TextHandler()
+
+        app.add_route('/media', self.MediaEcho())
+
+        client = falcon.testing.TestClient(app)
+        payload = 'Hello, Falcon!'
+        headers = {'Content-Type': 'text/plain'}
+        response = client.simulate_post('/media', body=payload, headers=headers)
+
+        assert response.status_code == 200
+        assert response.content_type == 'text/plain'
+        assert response.text == payload
+
+
 class TestRequestIDContext:
     @pytest.fixture
     def app(self, util):
