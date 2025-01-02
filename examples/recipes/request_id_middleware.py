29786cc9338d3e295d96674245c200441f35aa48
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2025-01-02 17:28:52 +0100

    docs(recipes): clean up request-id log recipes

--- examples/recipes/request_id_middleware.py
+++ examples/recipes/request_id_middleware.py
@@ -1,15 +1,13 @@
-# middleware.py
-
 from uuid import uuid4
 
-from context import ctx
+# Import the above context.py
+from my_app.context import ctx
 
 
 class RequestIDMiddleware:
     def process_request(self, req, resp):
         request_id = str(uuid4())
         ctx.request_id = request_id
-        req.context.request_id = request_id
 
     # It may also be helpful to include the ID in the response
     def process_response(self, req, resp, resource, req_succeeded):
