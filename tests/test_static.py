3387e0111c15472118f66cc733f69d7b6af1e778
Cycloctane <Cycloctane@outlook.com>
2025-04-29 18:04:36 +0800

    Revert "fix tests on windows"
    This reverts commit c674ba9ae371e8d030257d7c01e22267e98e3d5f.

--- tests/test_static.py
+++ tests/test_static.py
@@ -706,35 +706,28 @@ def test_etag(client, patch_open):
     assert resp.headers['ETag'] == f'"{int(mtime):x}-{len(resp.text):x}"'
 
 
-def test_if_none_match(client, patch_open):
+@pytest.mark.parametrize(
+    'if_none_match, is_304',
+    [
+        ('*', True),
+        ('"6782afce-21"', True),
+        ('"6782afce-21", "foo"', True),
+        ('W/"6782afce-21"', True),
+        ('"foo"', False),
+    ],
+)
+def test_if_none_match(client, patch_open, if_none_match, is_304):
     mtime = 1736617934.133701
     patch_open(mtime=mtime)
 
     client.app.add_static_route('/assets/', '/opt/somesite/assets')
-    etag = client.simulate_request(path='/assets/css/main.css').headers['ETag']
-
-    resp1 = client.simulate_request(
+    resp = client.simulate_request(
         path='/assets/css/main.css',
-        headers={'If-None-Match': etag},
+        headers={'If-None-Match': if_none_match},
     )
-    assert resp1.status == falcon.HTTP_304
-    assert resp1.text == ''
-
-    resp2 = client.simulate_request(
-        path='/assets/css/main.css',
-        headers={'If-None-Match': f'W/{etag}'},
-    )
-    assert resp2.status == falcon.HTTP_304
-
-    resp3 = client.simulate_request(
-        path='/assets/css/main.css',
-        headers={'If-None-Match': '*'},
-    )
-    assert resp3.status == falcon.HTTP_304
-
-    resp4 = client.simulate_request(
-        path='/assets/css/main.css',
-        headers={'If-None-Match': '"foo"'},
-    )
-    assert resp4.status == falcon.HTTP_200
-    assert resp4.text != ''
+    if is_304:
+        assert resp.status == falcon.HTTP_304
+        assert resp.text == ''
+    else:
+        assert resp.status == falcon.HTTP_200
+        assert resp.text == '/opt/somesite/assets/css/main.css'
