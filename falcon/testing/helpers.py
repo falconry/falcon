4bb7ae5af6d161f5a6de6fbede6bd1566f71db13
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2025-08-15 22:12:38 +0200

    chore: test just 127.0.0.1 in isolation in get_unused_port

--- falcon/testing/helpers.py
+++ falcon/testing/helpers.py
@@ -892,7 +892,7 @@ def get_unused_port() -> int:
     """
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('localhost', 0))
+        s.bind(('127.0.0.1', 0))
         return s.getsockname()[1]
 
 
