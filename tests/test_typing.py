8d504ad67b69c72ed7f189b14e704e26ad5a56ea
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2025-08-24 20:39:03 +0200

    test(typing): add typing cases for generic falcon.asgi.App

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -4,9 +4,8 @@ from dataclasses import dataclass
 from typing import Any, Optional
 from uuid import UUID
 
-import pytest
-
 import falcon
+import falcon.asgi
 import falcon.testing
 
 
@@ -24,6 +23,14 @@ class FancyResponse(falcon.Response):
     context_type = RichContext
 
 
+class FancyAsyncRequest(falcon.asgi.Request):
+    context_type = RichContext
+
+
+class FancyAsyncResponse(falcon.asgi.Response):
+    context_type = RichContext
+
+
 USERS = {
     'am9objoxMjM0': ('user', UUID('51e4b478-3825-4e46-9fd7-be7b61d616dc')),
     'dnl0YXM6MTIz': ('admin', UUID('5e50d2c4-1c52-42c7-b4c5-879d9bd390ee')),
@@ -35,67 +42,121 @@ def fancy_error_serializer(
 ) -> None:
     resp.content_type = falcon.MEDIA_JSON
     resp.media = ex.to_dict()
-    resp.media.update(fancy=True)
+    resp.media.update(fancy=True, asgi=False)
+
+
+def fancy_asgi_error_serializer(
+    req: FancyAsyncRequest, resp: falcon.asgi.Response, ex: falcon.HTTPError
+) -> None:
+    resp.content_type = falcon.MEDIA_JSON
+    resp.media = ex.to_dict()
+    resp.media.update(fancy=True, asgi=True)
+
+
+def _process_auth(req: falcon.Request, resp: falcon.Response) -> None:
+    if req.method == 'OPTIONS':
+        return
+
+    if req.auth:
+        for key, user_role in USERS.items():
+            if req.auth == f'Basic {key}':
+                req.context.role, req.context.userid = user_role
+                break
+        else:
+            raise falcon.HTTPUnauthorized()
 
 
 class AuthMiddlewareFancyRequest:
     def process_request(self, req: FancyRequest, resp: falcon.Response) -> None:
-        if req.method == 'OPTIONS':
-            return
+        _process_auth(req, resp)
 
-        if req.auth:
-            for key, user_role in USERS.items():
-                if req.auth == f'Basic {key}':
-                    req.context.role, req.context.userid = user_role
-                    break
-            else:
-                raise falcon.HTTPUnauthorized()
+    async def process_request_async(
+        self, req: FancyAsyncRequest, resp: falcon.asgi.Response
+    ) -> None:
+        _process_auth(req, resp)
 
 
 class AuthMiddlewareFancyBoth:
     def process_request(self, req: FancyRequest, resp: FancyResponse) -> None:
-        if req.method == 'OPTIONS':
-            return
+        _process_auth(req, resp)
 
-        if req.auth:
-            for key, user_role in USERS.items():
-                if req.auth == f'Basic {key}':
-                    req.context.role, req.context.userid = user_role
-                    break
-            else:
-                raise falcon.HTTPUnauthorized()
+    async def process_request_async(
+        self, req: FancyAsyncRequest, resp: FancyAsyncResponse
+    ) -> None:
+        _process_auth(req, resp)
 
 
-def sink_fancy_req(req: FancyRequest, resp: falcon.Response, **kwargs: Any) -> None:
+def _sink_impl(req: falcon.Request, resp: falcon.Response) -> None:
     userid: Optional[str] = str(req.context.userid) if req.context.userid else None
     resp.media = {'role': req.context.role, 'userid': userid}
     if req.path == '/not-found':
         raise falcon.HTTPNotFound()
 
 
+def sink_fancy_req(req: FancyRequest, resp: falcon.Response, **kwargs: Any) -> None:
+    _sink_impl(req, resp)
+
+
 def sink_fancy_both(req: FancyRequest, resp: FancyResponse, **kwargs: Any) -> None:
-    sink_fancy_req(req, resp, **kwargs)
+    _sink_impl(req, resp)
 
 
-def create_app_fancy_req() -> falcon.App:
+async def sink_fancy_async_req(
+    req: FancyAsyncRequest, resp: Optional[falcon.asgi.Response], **kwargs: Any
+) -> None:
+    if resp is not None:
+        _sink_impl(req, resp)
+
+
+async def sink_fancy_async_both(
+    req: FancyAsyncRequest, resp: Optional[FancyAsyncResponse], **kwargs: Any
+) -> None:
+    if resp is not None:
+        _sink_impl(req, resp)
+
+
+# NOTE(vytas): We don't use fixtures here because that is hard to marry to strict
+#   type checking, Mypy complains that "Untyped decorator makes function untyped".
+def test_app_fancy_req() -> None:
     app = falcon.App(request_type=FancyRequest)
     app.add_middleware(AuthMiddlewareFancyRequest())
     app.add_sink(sink_fancy_req)
     app.set_error_serializer(fancy_error_serializer)
-    return app
+
+    _exercise_app(app)
 
 
-def create_app_fancy_both() -> falcon.App:
+def test_app_fancy_both() -> None:
     app = falcon.App(request_type=FancyRequest, response_type=FancyResponse)
     app.add_middleware(AuthMiddlewareFancyBoth())
     app.add_sink(sink_fancy_both)
     app.set_error_serializer(fancy_error_serializer)
-    return app
+
+    _exercise_app(app)
 
 
-@pytest.mark.parametrize('create_app', [create_app_fancy_req, create_app_fancy_both])
-def test_fancy_wsgi_app(create_app) -> None:
-    client = falcon.testing.TestClient(create_app())
+def test_app_fancy_async_req() -> None:
+    app = falcon.asgi.App(request_type=FancyAsyncRequest)
+    app.add_middleware(AuthMiddlewareFancyRequest())
+    app.add_sink(sink_fancy_async_req)
+    app.set_error_serializer(fancy_asgi_error_serializer)
+
+    _exercise_app(app)
+
+
+def test_app_fancy_async_both() -> None:
+    app = falcon.asgi.App(
+        request_type=FancyAsyncRequest, response_type=FancyAsyncResponse
+    )
+    app.add_middleware(AuthMiddlewareFancyBoth())
+    app.add_sink(sink_fancy_async_both)
+    app.set_error_serializer(fancy_asgi_error_serializer)
+
+    _exercise_app(app)
+
+
+def _exercise_app(app: falcon.App[Any, Any]) -> None:
+    client = falcon.testing.TestClient(app)
 
     result1 = client.get()
     assert result1.status_code == 200
@@ -116,4 +177,8 @@ def test_fancy_wsgi_app(create_app) -> None:
 
     result4 = client.get('/not-found')
     assert result4.status_code == 404
-    assert result4.json == {'fancy': True, 'title': '404 Not Found'}
+    assert result4.json == {
+        'asgi': isinstance(client.app, falcon.asgi.App),
+        'fancy': True,
+        'title': '404 Not Found',
+    }
