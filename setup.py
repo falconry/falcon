e29c8492d0c4d6607b21cd4b62bf033c8a7d191c
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2025-09-06 10:31:06 +0200

    chore(platform): drop Python 3.8 support

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if HAS_CYTHON and IS_CPYTHON and not DISABLE_EXTENSION:
     ]
 
     cython_package_names = ('falcon.cyutil',)
-    # NOTE(vytas): Now that all our codebase is Python 3.8+, specify the
+    # NOTE(vytas): Now that all our codebase is Python 3.9+, specify the
     #   Python 3 language level for Cython as well to avoid any surprises.
     cython_directives = {'language_level': '3', 'annotation_typing': False}
 
