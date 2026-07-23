9e5c3714e83740a98ce02152a4ee79b788510d74
KetanHegde <ketanhegde2003@gmail.com>
2026-07-23 22:45:38 +0530

    Update falcon/request.py
    Co-authored-by: Federico Caselli <cfederico87@gmail.com>

--- falcon/request.py
+++ falcon/request.py
@@ -647,7 +647,7 @@ class Request:
         # include it even in that case.
         try:
             # TODO(0xMattB): Implement advanced typing to type as 'str' (see PR #2599)
-            root_path = self.env['SCRIPT_NAME']
+            root_path: str = self.env['SCRIPT_NAME']
             if not root_path.isascii():
                 root_path = root_path.encode('iso-8859-1').decode('utf-8', 'replace')
             return root_path  # type: ignore[no-any-return]
